d5cc94761bf4f0639f8e04534269663d13b998a9
Berthold Roiser <berthold.roiser@gmail.com>
2018-01-19 14:14:26 +0100

    create blockchain-network

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # iVolunteerPrototype
+
+docker ps -aq | xargs docker rm -f
+docker images -aq | xargs docker rmi -f
---
+++ blockchain-network/lib/logic.js
@@ -0,0 +1,23 @@
+'use strict';
+/**
+ * Write your transction processor functions here
+ */
+
+/**
+ * Sample transaction
+ * @param {at.jku.cis.ChangeAssetValue} changeAssetValue
+ * @transaction
+ */
+function onChangeAssetValue(changeAssetValue) {
+    var assetRegistry;
+    var id = changeAssetValue.relatedAsset.assetId;
+    return getAssetRegistry('at.jku.cis.SampleAsset')
+        .then(function(ar) {
+            assetRegistry = ar;
+            return assetRegistry.get(id);
+        })
+        .then(function(asset) {
+            asset.value = changeAssetValue.newValue;
+            return assetRegistry.update(asset);
+        });
+}
