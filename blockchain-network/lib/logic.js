e725e476b37a465615d61e2616c58483dbf39640
Philipp Starzer <philipp.starzer@gmail.com>
2018-01-22 13:02:01 +0100

    asdfsadf

--- blockchain-network/lib/logic.js
+++ blockchain-network/lib/logic.js
@@ -3,3 +3,21 @@
  * Write your transction processor functions here
  */
 
+/**
+ * Place an order for a vehicle
+ * @param {at.jku.cis.CreateTask} createTask - the CreateTask transaction
+ * @transaction
+ */
+function createTask(createTask) {
+    console.log('createTask');
+}
+
+
+/**
+ * Update the status of a task
+ * @param {at.jku.cis.UpdateTaskStatus} updateTaskStatus - the UpdateTaskStatus transaction
+ * @transaction
+ */
+function updateTaskStatus(updateTaskStatus) {
+    console.log('updateTaskStatus');
+}
