14a1b84ac71d935278dc436c4c9b289b9ec9a978
Philipp Starzer <philipp.starzer@gmail.com>
2018-01-25 17:21:22 +0100

    transaction functions

--- blockchain-network/lib/logic.js
+++ blockchain-network/lib/logic.js
@@ -3,16 +3,17 @@
  * Write your transction processor functions here
  */
 
+var NS = 'at.jku.cis';
+
 /**
  * Create a new task
- * @param {at.jku.cis.CreateTask} createTask - the CreateTask transaction
+ * @param {at.jku.cis.createTask} createTask - the createTask transaction
  * @transaction
  */
 function createTask(createTask) {
     console.log('createTask');
 
     var factory = getFactory();
-    var NS = 'at.jku.cis';
 
     var task = factory.newResource(NS, 'Task', createTask.taskId);
     task.taskStatus = 'CREATED';
@@ -25,20 +26,106 @@ function createTask(createTask) {
             return registry.add(task);
         })
         .then(function(){
-    		var createTaskEvent = factory.newEvent(NS, 'CreateTaskEvent');
-            createTaskEvent.taskId = task.taskId;
-            createTaskEvent.description = task.description;
+    		var createTaskEvent = factory.newEvent(NS, 'createTaskEvent');
+            createTaskEvent.task = task;
     		emit(createTaskEvent);
     	});
 
 }
 
-
 /**
- * Update the status of a task
- * @param {at.jku.cis.UpdateTaskStatus} updateTaskStatus - the UpdateTaskStatus transaction
+ * Reserve volunteer for a task
+ * @param {at.jku.cis.reserveTask} reserveTask - the reserveTask transaction
  * @transaction
  */
-function updateTaskStatus(updateTaskStatus) {
-    console.log('updateTaskStatus');
+function reserveTask(reserveTask) {
+    console.log('reserveTask');
+    var factory = getFactory();
+
+    var task = reserveTask.task;
+    task.taskStatus = 'RESERVED';
+
+    if (!task.reservedVolunteers) {
+        task.reservedVolunteers = [];
+    }
+    task.reservedVolunteers.push(reserveTask.volunteer);
+
+    return getAssetRegistry(NS + '.Task')
+        .then(function (registry) {
+            return registry.update(task);
+        })
+        .then(function(){
+    		var reserveTaskEvent = factory.newEvent(NS, 'reserveTaskEvent');
+            reserveTaskEvent.task = task;
+            reserveTaskEvent.volunteer = reserveTask.volunteer;
+    		emit(reserveTaskEvent);
+    	});
+
+    
+}
+
+/**
+ * Assign a task reservation to volunteer(s)
+ * @param {at.jku.cis.assignTask} assignTask - the assignTask transaction
+ * @transaction
+ */
+function assignTask(assignTask) {
+    console.log('assignTask');
+    var factory = getFactory();
+
+    var task = assignTask.task;
+    task.taskStatus = 'ASSIGNED';
+
+    // make taskPerformer to an array if it isn't
+    if (!task.taskPerformer) {
+        task.taskPerformer = [];
+    }
+
+    // TODO: error handling überprüfen!
+             
+    // check if volunteer from assignTask.taskPerformer is also in task.reservedVolunteers
+    while(assignTask.taskPerformer.length > 0) {
+        var volunteer = assignTask.taskPerformer.pop();
+
+        // if(task.reservedVolunteers.contains(volunteer))
+        if(task.reservedVolunteers.indexOf(volunteer) != -1) {
+            task.taskPerformer.push(volunteer);
+        }else {
+            throw new Error('Volunteer not in reservedVolunteers array');
+        }
+    }
+    
+    return getAssetRegistry(NS + '.Task')
+        .then(function (registry) {
+            return registry.update(task);
+        })
+        .then(function(){
+    		var assignTaskEvent = factory.newEvent(NS, 'assignTaskEvent');
+            assignTaskEvent.task = task;
+            assignTaskEvent.taskPerformer = task.taskPerformer;
+    		emit(assignTaskEvent);
+    	});
+}
+
+/**
+ * Finish a task
+ * @param {at.jku.cis.finishTask} finishTask - the finishTask transaction
+ * @transaction
+ */
+function finishTask(finishTask) {
+    console.log('finishTask');
+    var factory = getFactory();
+
+    var task = finishTask.task;
+    task.taskStatus = 'FINISHED';
+
+    return getAssetRegistry(NS + '.Task')
+        .then(function (registry) {
+            return registry.update(task);
+        })
+        .then(function(){
+    		var finishTaskEvent = factory.newEvent(NS, 'finishTaskEvent');
+            finishTaskEvent.task = task;
+    		emit(finishTaskEvent);
+    	});
 }
