66ca91913275e208c81a37cde471bd5dc13e8a6f
Berthold Roiser <berthold.roiser@gmail.com>
2018-02-19 13:44:33 +0100

    marterial module, style.css

--- volunteer-client/src/app/app.module.ts
+++ volunteer-client/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AppMaterialModule} from './app-material.module';
 
 
 @NgModule({
@@ -10,9 +11,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    AppMaterialModule,
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
