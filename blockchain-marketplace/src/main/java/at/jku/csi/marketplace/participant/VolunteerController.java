a6731310ff12ad3e2c346cebfa243c6179bec1f0
ntsmwk <markus.weissenbek@gmx.at>
2018-02-23 09:23:22 +0100

    implemented TaskType, Employee, Volunteer controller

---
+++ blockchain-marketplace/src/main/java/at/jku/csi/marketplace/participant/VolunteerController.java
@@ -0,0 +1,31 @@
+package at.jku.csi.marketplace.participant;
+
+import javax.ws.rs.NotAcceptableException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import at.jku.csi.marketplace.task.Task;
+
+@RestController
+public class VolunteerController {
+
+	@Autowired
+	private VolunteerRepository volunteerRepository;
+	
+	@PostMapping("/volunteer/{id}/profile")
+	public Volunteer updateVolunteer(@PathVariable("id") String id, @RequestBody Volunteer volunteer) {
+		if (volunteerRepository.exists(id)) {
+			throw new NotAcceptableException();
+		}
+		return volunteerRepository.save(volunteer);
+	}	
+	
+	
+	
+	
+}
