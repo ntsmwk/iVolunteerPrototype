42018999ba2c425637f5679b088cbf0e4a8d027d
ntsmwk <markus.weissenbek@gmx.at>
2018-03-05 12:47:31 +0100

    fixed enum conversion for taskOperation

--- marketplace/marketplace-application/src/main/java/at/jku/csi/marketplace/task/interaction/TaskInteractionRepository.java
+++ marketplace/marketplace-application/src/main/java/at/jku/csi/marketplace/task/interaction/TaskInteractionRepository.java
@@ -17,4 +17,7 @@ public interface TaskInteractionRepository extends MongoRepository<TaskInteracti
 	@Query("{ 'participant': {'$ref': 'volunteer', '$id': ?0 } }")
 	List<TaskInteraction> findByVolunteer(String volunteertId);
 
+	
+	@Query(" 'participant': {'$ref': 'volunteer', '$id': ?0}, 'task': {'$ref': 'task', '$id': ?1} }")
+	List<TaskInteraction> findByVolunteerAndTask (String volunteerId, String taskId);
 }
