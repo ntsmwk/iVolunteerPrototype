7260ef652853548f750fb1643da40c8307e560c9
ntsmwk <markus.weissenbek@gmx.at>
2018-03-05 16:58:25 +0100

    merge

--- marketplace-client/src/app/app-routing.module.ts
+++ marketplace-client/src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {TaskListComponent} from './task/list/task-list.component';
 import {TaskAvailableComponent} from './task/available/task-available.component';
@@ -8,23 +7,28 @@ import {TaskTypeCreateComponent} from './task-type/create/task-type-create.compo
 import {TaskDetailsComponent} from './task/details/task-details.component';
 import {LoginComponent} from './login/login.component';
 import {TaskAssignComponent} from './task/assign/task-assign.component';
+import {TokenGuard} from './login/token.guard';
+import {EmployeeGuard} from './participant/employee.guard';
+import {VolunteerGuard} from './participant/volunteer.guard';
+import {NgModule} from '@angular/core';
+
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: 'task', component: TaskCreateComponent, canActivate: [LoginGuard]},
-  {path: 'task/:id/details', component: TaskDetailsComponent, canActivate: [LoginGuard]},
-  {path: 'tasks', component: TaskListComponent, canActivate: [LoginGuard]},
-  {path: 'task/assign/:id', component: TaskAssignComponent, canActivate: [LoginGuard]},
-  {path: 'available', component: TaskAvailableComponent, canActivate: [LoginGuard]},
-  {path: 'taskType', component: TaskTypeCreateComponent, canActivate: [LoginGuard]},
-  {path: 'taskTypes', component: TaskTypeListComponent, canActivate: [LoginGuard]},
-  {path: '', redirectTo: '/tasks', pathMatch: 'full'}
-
-];
+  {path: 'task', component: TaskCreateComponent, canActivate: [TokenGuard, EmployeeGuard]},
+  {path: 'task/:id/details', component: TaskDetailsComponent, canActivate: [TokenGuard]},
+  {path: 'task/assign/:id', component: TaskAssignComponent, canActivate: [EmployeeGuard]},
+  {path: 'tasks', component: TaskListComponent, canActivate: [TokenGuard]},
+  {path: 'available', component: TaskAvailableComponent, canActivate: [TokenGuard, VolunteerGuard]},
+  {path: 'taskType', component: TaskTypeCreateComponent, canActivate: [TokenGuard, EmployeeGuard]},
+  {path: 'taskTypes', component: TaskTypeListComponent, canActivate: [TokenGuard, EmployeeGuard]},
+  {path: '', redirectTo: '/tasks', pathMatch: 'full'},
+  {path: '**', redirectTo: '/tasks'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule {
 }
