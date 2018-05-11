ae44df24f6795f15b589dc67f124a9ab012856db
Berthold Roiser <berthold.roiser@gmail.com>
2018-05-11 13:08:46 +0200

    refactoring

--- marketplace/marketplace-application/src/main/java/at/jku/cis/marketplace/trustifier/VerifierRestClient.java
+++ marketplace/marketplace-application/src/main/java/at/jku/cis/marketplace/trustifier/VerifierRestClient.java
@@ -16,7 +16,11 @@ import at.jku.cis.marketplace.task.Task;
 @Service
 public class VerifierRestClient {
 
-	private static final String TRUSTIFIER_CONTRACTOR_TASK = "{0}/trustifier/verifier/{1}";
+	private static final String TASK = "task";
+	private static final String TASK_ENTRY = "taskEntry";
+	private static final String COMPETENCE_ENTRY = "competenceEntry";
+
+	private static final String VERIFIER_URI = "{0}/trustifier/verifier/{1}";
 
 	@Value("${marketplace.trustifier.uri}")
 	private URI trustifierURI;
@@ -25,22 +29,22 @@ public class VerifierRestClient {
 	private RestTemplate restTemplate;
 
 	public boolean verifyTask(Task task) {
-		String requestURI = buildContractorRequestURI("task");
+		String requestURI = buildContractorRequestURI(TASK);
 		return restTemplate.postForObject(requestURI, task, Boolean.class).booleanValue();
 	}
 
 	public boolean verifyTaskEntry(TaskEntry taskEntry) {
-		String requestURI = buildContractorRequestURI("taskEntry");
+		String requestURI = buildContractorRequestURI(TASK_ENTRY);
 		return restTemplate.postForObject(requestURI, taskEntry, Boolean.class).booleanValue();
 	}
 
 	public boolean verifyCompetenceEntry(CompetenceEntry competenceEntry) {
-		String requestURI = buildContractorRequestURI("competenceEntry");
+		String requestURI = buildContractorRequestURI(COMPETENCE_ENTRY);
 		return restTemplate.postForObject(requestURI, competenceEntry, Boolean.class).booleanValue();
 	}
 
 	private String buildContractorRequestURI(String requestPath) {
-		return format(TRUSTIFIER_CONTRACTOR_TASK, trustifierURI, requestPath);
+		return format(VERIFIER_URI, trustifierURI, requestPath);
 	}
 
 }
