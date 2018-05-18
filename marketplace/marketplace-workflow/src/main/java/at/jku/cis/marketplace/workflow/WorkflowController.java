0dfe24bb24643a5d0547b639e1d25c4306fce424
Markus Weißenbek <markus.weissenbek@chatvisor.com>
2018-05-18 13:43:56 +0200

    workflow new

--- marketplace/marketplace-workflow/src/main/java/at/jku/cis/marketplace/workflow/WorkflowController.java
+++ marketplace/marketplace-workflow/src/main/java/at/jku/cis/marketplace/workflow/WorkflowController.java
@@ -1,21 +1,18 @@
 package at.jku.cis.marketplace.workflow;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.activiti.engine.ProcessEngine;
-import org.activiti.engine.impl.bpmn.parser.EventSubscriptionDeclaration;
-import org.activiti.engine.impl.persistence.entity.ExecutionEntity;
-import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
-import org.activiti.engine.impl.pvm.process.ActivityImpl;
-import org.activiti.engine.runtime.Execution;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.task.Task;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +24,13 @@ public class WorkflowController {
 	@Autowired
 	private ProcessEngine processEngine;
 
-	// curl -H "Content-Type: application/json" -d '' http://localhost:8080/workflow/myProcess?taskId=abcdedfg
+	@Autowired
+	private TaskService taskService;
+
+	@Autowired
+	private WorkflowStepService workflowStepService;
+
+	// curl -H "Content-Type: application/json" -d '' http://localhost:8080/workflow/myProcessMwe?taskId=abcdedfg
 	@PostMapping("/{processKey}")
 	public String startWorkflow(@PathVariable("processKey") String processKey, @RequestParam("taskId") String taskId) {
 		Map<String, Object> params = new HashMap<>();
@@ -35,52 +38,30 @@ public class WorkflowController {
 		return processEngine.getRuntimeService().startProcessInstanceByKey(processKey, params).getProcessInstanceId();
 	}
 
-	// curl -H "Content-Type: application/json" http://localhost:8080/workflow/myProcess/5/task
-	@GetMapping("/{processKey}/{instanceId}/event")
-	public List<String> getTasksByInstanceId(@PathVariable("processKey") String processKey,
+	// curl -H "Content-Type: application/json" http://localhost:8080/workflow/myProcessMwe/8/task
+	@GetMapping("/{processKey}/{instanceId}/task")
+	public List<WorkflowStep> getTasksByInstanceId(@PathVariable("processKey") String processKey,
 			@PathVariable("instanceId") String instanceId) {
-		return retrieveEventsByProcessKeyAndInstanceId(processKey, instanceId);
+		
+		return workflowStepService
+				.getNextWorkflowSteps(retrieveActiveTaskByProcessKeyAndInstanceId(processKey, instanceId));
 	}
 
-	// curl -H "Content-Type: application/json" -d '' http://localhost:8080/workflow/myProcess/17/task/21
-	@PostMapping("/{processKey}/{instanceId}/event/{eventName}")
+	// curl -H "Content-Type: application/json" -d '' http://localhost:8080/workflow/myProcessMwe/8/task
+	@PostMapping("/{processKey}/{instanceId}/task")
 	public void completeTask(@PathVariable("processKey") String processKey,
-			@PathVariable("instanceId") String instanceId, @PathVariable("eventName") String eventName) {
-		List<Execution> executions = processEngine.getRuntimeService().createExecutionQuery()
-				.processInstanceId(instanceId).signalEventSubscriptionName(eventName).list();
-		if (executions.isEmpty()) {
+			@PathVariable("instanceId") String instanceId, @RequestBody WorkflowStep workflowStep) {
+		Task task = retrieveActiveTaskByProcessKeyAndInstanceId(processKey, instanceId);
+		if (StringUtils.equals(task.getId(), workflowStep.getTaskId())) {
 			throw new UnsupportedOperationException();
 		}
 
-		for (Execution execution : executions) {
-			processEngine.getRuntimeService().signalEventReceived(eventName, execution.getId());
-		}
+		Map<String, Object> params = new HashMap<>();
+		params.put("label", workflowStep.getLabel());
+		taskService.complete(workflowStep.getTaskId(), params);
 	}
 
-	private List<String> retrieveEventsByProcessKeyAndInstanceId(String processKey, String instanceId) {
-		List<String> events = new ArrayList<>();
-		List<Execution> executions = findExectutionsByProcessInstanceId(instanceId);
-		executions.forEach(execution -> events.addAll(extractEventsOfExecution(execution)));
-		return events.stream().distinct().collect(Collectors.toList());
+	private Task retrieveActiveTaskByProcessKeyAndInstanceId(String processKey, String instanceId) {
+		return taskService.createTaskQuery().processInstanceId(instanceId).active().singleResult();
 	}
-
-	private List<Execution> findExectutionsByProcessInstanceId(String instanceId) {
-		return processEngine.getRuntimeService().createExecutionQuery().processInstanceId(instanceId).list().stream()
-				.filter(t -> t.getParentId() != null).collect(Collectors.toList());
-	}
-
-	private List<String> extractEventsOfExecution(Execution execution) {
-		List<EventSubscriptionDeclaration> eventDefinitions = extractEventDefinitions(execution);
-		return eventDefinitions.stream().map(event -> event.getEventName()).collect(Collectors.toList());
-	}
-
-	@SuppressWarnings("unchecked")
-	private List<EventSubscriptionDeclaration> extractEventDefinitions(Execution execution) {
-		ExecutionEntity executionEntity = (ExecutionEntity) execution;
-		ProcessDefinitionEntity processDefinition = (ProcessDefinitionEntity) processEngine.getRepositoryService()
-				.getProcessDefinition(executionEntity.getProcessDefinitionId());
-		ActivityImpl activity = processDefinition.findActivity(execution.getActivityId());
-		return (List<EventSubscriptionDeclaration>) activity.getProperties().get("eventDefinitions");
-	}
-
 }
