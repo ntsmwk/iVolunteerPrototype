928f7dff32360c05058202775fca7e7718483e3a
Markus Weißenbek <markus.weissenbek@chatvisor.com>
2018-05-18 15:43:45 +0200

    get all deployed workflows, and their keys works now

--- marketplace/marketplace-workflow/src/main/java/at/jku/cis/marketplace/workflow/WorkflowController.java
+++ marketplace/marketplace-workflow/src/main/java/at/jku/cis/marketplace/workflow/WorkflowController.java
@@ -3,12 +3,15 @@ package at.jku.cis.marketplace.workflow;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.task.Task;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,27 +25,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class WorkflowController {
 
 	@Autowired
-	private ProcessEngine processEngine;
-
-	@Autowired
 	private TaskService taskService;
 
 	@Autowired
+	private RuntimeService runtimeService;
+
+	@Autowired
+	private RepositoryService repositoryService;
+
+	@Autowired
 	private WorkflowStepService workflowStepService;
+
+	@GetMapping()
+	public List<String> getWorkflowProcesses() {
+		return repositoryService.createProcessDefinitionQuery().list().stream()
+				.map(processDefinition -> processDefinition.getKey()).collect(Collectors.toList());
+	}
 
 	// curl -H "Content-Type: application/json" -d '' http://localhost:8080/workflow/myProcessMwe?taskId=abcdedfg
 	@PostMapping("/{processKey}")
 	public String startWorkflow(@PathVariable("processKey") String processKey, @RequestParam("taskId") String taskId) {
 		Map<String, Object> params = new HashMap<>();
 		params.put("taskId", taskId);
-		return processEngine.getRuntimeService().startProcessInstanceByKey(processKey, params).getProcessInstanceId();
+		return runtimeService.startProcessInstanceByKey(processKey, params).getProcessInstanceId();
 	}
 
 	// curl -H "Content-Type: application/json" http://localhost:8080/workflow/myProcessMwe/8/task
 	@GetMapping("/{processKey}/{instanceId}/task")
 	public List<WorkflowStep> getTasksByInstanceId(@PathVariable("processKey") String processKey,
 			@PathVariable("instanceId") String instanceId) {
-		
 		return workflowStepService
 				.getNextWorkflowSteps(retrieveActiveTaskByProcessKeyAndInstanceId(processKey, instanceId));
 	}
@@ -51,14 +62,21 @@ public class WorkflowController {
 	@PostMapping("/{processKey}/{instanceId}/task")
 	public void completeTask(@PathVariable("processKey") String processKey,
 			@PathVariable("instanceId") String instanceId, @RequestBody WorkflowStep workflowStep) {
+
 		Task task = retrieveActiveTaskByProcessKeyAndInstanceId(processKey, instanceId);
-		if (StringUtils.equals(task.getId(), workflowStep.getTaskId())) {
+		if (!StringUtils.equals(task.getId(), workflowStep.getTaskId())) {
 			throw new UnsupportedOperationException();
 		}
 
 		Map<String, Object> params = new HashMap<>();
 		params.put("label", workflowStep.getLabel());
 		taskService.complete(workflowStep.getTaskId(), params);
+	}
+
+	@DeleteMapping("/{processKey}/{instanceId}")
+	public void cancelWorkflow(@PathVariable("processKey") String processKey,
+			@PathVariable("instanceId") String instanceId) {
+		runtimeService.deleteProcessInstance(instanceId, "Workflow is aborted");
 	}
 
 	private Task retrieveActiveTaskByProcessKeyAndInstanceId(String processKey, String instanceId) {
