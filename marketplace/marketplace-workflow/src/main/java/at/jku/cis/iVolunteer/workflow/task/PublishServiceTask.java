8b114729b84832d9c4d1cb4c15f9fbd388e7d726
Berthold Roiser <berthold.roiser@gmail.com>
2018-05-28 15:35:09 +0200

    removed workflow 2, adapt standard workflow

--- marketplace/marketplace-workflow/src/main/java/at/jku/cis/iVolunteer/workflow/task/PublishServiceTask.java
+++ marketplace/marketplace-workflow/src/main/java/at/jku/cis/iVolunteer/workflow/task/PublishServiceTask.java
@@ -1,5 +1,8 @@
 package at.jku.cis.iVolunteer.workflow.task;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +25,10 @@ public class PublishServiceTask implements JavaDelegate {
 		String token = delegateExecution.getVariable("accessToken", String.class);
 		System.out.println(this.getClass().getName() +"{taskId: "+ taskId+"}");
 		
+		Set<String> volunteers = new HashSet<String>();
+		volunteers.add("broiser");
+		volunteers.add("mweissenbek");
+		delegateExecution.setVariable("volunteers", volunteers);
 		marketplaceRestClient.publishTask(taskId, token);
 	}
 }
