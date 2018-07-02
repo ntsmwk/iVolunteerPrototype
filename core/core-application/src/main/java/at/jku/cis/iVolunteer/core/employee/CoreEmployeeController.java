1b06419d167f47519652138068e67df0578bdf7f
Berthold Roiser <berthold.roiser@gmail.com>
2018-07-02 12:46:40 +0200

    refactoring, fixed login bug

--- core/core-application/src/main/java/at/jku/cis/iVolunteer/core/employee/CoreEmployeeController.java
+++ core/core-application/src/main/java/at/jku/cis/iVolunteer/core/employee/CoreEmployeeController.java
@@ -1,28 +1,25 @@
 package at.jku.cis.iVolunteer.core.employee;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import at.jku.cis.iVolunteer.model.core.participant.CoreEmployee;
-import at.jku.cis.iVolunteer.model.marketplace.Marketplace;
+import at.jku.cis.iVolunteer.mapper.core.participant.CoreEmployeeMapper;
+import at.jku.cis.iVolunteer.model.core.participant.dto.CoreEmployeeDTO;
 
 @RestController("/employee")
 public class CoreEmployeeController {
 
 	@Autowired
+	private CoreEmployeeMapper coreEmployeeMapper;
+
+	@Autowired
 	private CoreEmployeeRepository coreEmployeeRepository;
 
 	@GetMapping("/{coreEmployeeId}")
-	public CoreEmployee getCoreEmployee(@PathVariable("coreEmployeeId") String coreEmployeeId) {
-		return coreEmployeeRepository.findOne(coreEmployeeId);
+	public CoreEmployeeDTO getCoreEmployee(@PathVariable("coreEmployeeId") String coreEmployeeId) {
+		return coreEmployeeMapper.toDTO(coreEmployeeRepository.findOne(coreEmployeeId));
 	}
 
-	@GetMapping("/{coreEmployeeId}/marketplaces")
-	public List<Marketplace> getRegisteredMarketplaces(@PathVariable("coreEmployeeId") String coreEmployeeId) {
-		return coreEmployeeRepository.findOne(coreEmployeeId).getRegisteredMarketplaces();
-	}
 }
--- core/core-application/src/main/java/at/jku/cis/iVolunteer/core/volunteer/CoreVolunteerController.java
+++ core/core-application/src/main/java/at/jku/cis/iVolunteer/core/volunteer/CoreVolunteerController.java
@@ -7,22 +7,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.jku.cis.iVolunteer.mapper.core.participant.CoreVolunteerMapper;
+import at.jku.cis.iVolunteer.mapper.marketplace.MarketplaceMapper;
 import at.jku.cis.iVolunteer.model.core.participant.CoreVolunteer;
-import at.jku.cis.iVolunteer.model.marketplace.Marketplace;
+import at.jku.cis.iVolunteer.model.core.participant.dto.CoreVolunteerDTO;
+import at.jku.cis.iVolunteer.model.marketplace.dto.MarketplaceDTO;
 
 @RestController("/volunteer")
 public class CoreVolunteerController {
 
 	@Autowired
+	private MarketplaceMapper marketplaceMapper;
+
+	@Autowired
+	private CoreVolunteerMapper coreVolunteerMapper;
+
+	@Autowired
 	private CoreVolunteerRepository coreVolunteerRepository;
 
-	@GetMapping("/{coreVolunteerId}")
-	public CoreVolunteer getCoreVolunteer(@PathVariable("coreVolunteerId") String coreVolunteerId) {
-		return coreVolunteerRepository.findOne(coreVolunteerId);
+	@GetMapping("/{volunteerId}")
+	public CoreVolunteerDTO getCoreVolunteer(@PathVariable("volunteerId") String volunteerId) {
+		return coreVolunteerMapper.toDTO(coreVolunteerRepository.findOne(volunteerId));
 	}
 
-	@GetMapping("/{coreVolunteerId}/marketplaces")
-	public List<Marketplace> getRegisteredMarketplaces(@PathVariable("coreVolunteerId") String coreVolunteerId) {
-		return coreVolunteerRepository.findOne(coreVolunteerId).getRegisteredMarketplaces();
+	@GetMapping("/{volunteerId}/marketplace")
+	public List<MarketplaceDTO> getRegisteredMarketplaces(@PathVariable("volunteerId") String volunteerId) {
+		CoreVolunteer volunteer = coreVolunteerRepository.findOne(volunteerId);
+		return marketplaceMapper.toDTOs(volunteer.getRegisteredMarketplaces());
 	}
 }
