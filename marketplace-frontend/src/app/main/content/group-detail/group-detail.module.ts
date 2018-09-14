d0f815f6f07cebd45c6c3502140772db458929d1
Berthold Roiser <berthold.roiser@gmail.com>
2018-09-14 10:57:42 +0200

    group-detail sidenav

--- marketplace-frontend/src/app/main/content/group-detail/group-detail.module.ts
+++ marketplace-frontend/src/app/main/content/group-detail/group-detail.module.ts
@@ -1,17 +1,9 @@
 import {NgModule} from '@angular/core';
 import {Route, RouterModule} from '@angular/router';
-import {
-  MatButtonModule,
-  MatCheckboxModule,
-  MatDatepickerModule,
-  MatDividerModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatNativeDateModule,
-  MatSelectModule
-} from '@angular/material';
+import {MatSidenavModule} from '@angular/material';
 import {FuseSharedModule} from '@fuse/shared.module';
 import {FuseGroupDetailComponent} from './group-detail.component';
+import {FuseGroupMemberComponent} from './sidenavs/group-member/group-member.component';
 
 const routes: Route[] = [
   {path: ':groupId', component: FuseGroupDetailComponent}
@@ -19,18 +11,14 @@ const routes: Route[] = [
 
 @NgModule({
   declarations: [
-    FuseGroupDetailComponent
+    FuseGroupDetailComponent,
+
+    FuseGroupMemberComponent
   ],
   imports: [
     RouterModule.forChild(routes),
-    MatButtonModule,
-    MatCheckboxModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatDividerModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
+
+    MatSidenavModule,
 
     FuseSharedModule
   ]
