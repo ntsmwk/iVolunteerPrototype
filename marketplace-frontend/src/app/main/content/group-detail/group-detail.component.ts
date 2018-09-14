c87f4fe4cefaa670b0a63ec4eff60c0b908b84e7
Berthold Roiser <berthold.roiser@gmail.com>
2018-09-14 12:41:58 +0200

    thiem austria

--- marketplace-frontend/src/app/main/content/group-detail/group-detail.component.ts
+++ marketplace-frontend/src/app/main/content/group-detail/group-detail.component.ts
@@ -7,6 +7,33 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class FuseGroupDetailComponent implements OnInit {
 
+  group = {
+    posts: [
+      {
+        'user': {
+          'name': 'Garry Newman',
+          'avatar': 'assets/images/avatars/garry.jpg'
+        },
+        'message': 'Hey Guys, does somebody want to join me in the marketplace "Marketplace X"? "Marketplace Description"',
+        'time': '32 minutes ago',
+        'type': 'invitation',
+        'like': 5,
+        'share': 1
+      },
+      {
+        'user': {
+          'name': 'Garry Newman',
+          'avatar': 'assets/images/avatars/garry.jpg'
+        },
+        'message': 'Some impressions of our last project. We are always looking for interessted and motivation people. ;)',
+        'time': '3 hours ago',
+        'type': 'something',
+        'like': 5,
+        'share': 0
+      }
+    ]
+  };
+
   constructor(private route: ActivatedRoute) {
   }
 
@@ -15,7 +42,5 @@ export class FuseGroupDetailComponent implements OnInit {
   }
 
   private loadGroup(groupId: string) {
-    console.log(groupId);
-
   }
 }
