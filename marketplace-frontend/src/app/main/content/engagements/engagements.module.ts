34d4e880d87accf3844b3aba23e5344e1d320037
Philipp Starzer <philipp.starzer@gmail.com>
2018-09-24 09:59:48 +0200

    engagement tab anpassungen

--- marketplace-frontend/src/app/main/content/engagements/engagements.module.ts
+++ marketplace-frontend/src/app/main/content/engagements/engagements.module.ts
@@ -1,16 +1,26 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-import {MatTabsModule} from '@angular/material';
+import {
+  MatButtonModule, MatDatepickerModule, MatDividerModule, MatExpansionModule, MatFormFieldModule, MatIconModule, MatInputModule, MatProgressBarModule, MatSidenavModule,
+  MatSlideToggleModule,
+  MatTabsModule, MatToolbarModule
+} from '@angular/material';
 
 import {FuseSharedModule} from '@fuse/shared.module';
-import {FuseCalendarModule} from './calendar/calendar.module';
-import {FuseProjectsModule} from './projects/projects.module';
 import {FuseEngagementsComponent} from './engagements.component';
 import { CollaborationsComponent } from './collaborations/collaborations.component';
 import { ContributionsComponent } from './contributions/contributions.component';
 import { EncouragementsComponent } from './encouragements/encouragements.component';
 import { OpportunitiesComponent } from './opportunities/opportunities.component';
+import {FuseProjectMembersModule} from '../@shared/project-members/project-members.module';
+import {FuseProjectTaskListModule} from '../@shared/project-task-list/project-task-list.module';
+import {FuseTruncatePipeModule} from '../_pipe/truncate-pipe.module';
+import {CalendarModule} from 'angular-calendar';
+import {ColorPickerModule} from 'ngx-color-picker';
+import {NgbModalModule} from '@ng-bootstrap/ng-bootstrap';
+import {FuseConfirmDialogModule} from '../../../../@fuse/components';
+import {FuseCalendarComponent} from './calendar/calendar.component';
 
 const routes: Routes = [
   {path: '', component: FuseEngagementsComponent},
@@ -24,16 +34,38 @@ const routes: Routes = [
     CollaborationsComponent,
     ContributionsComponent,
     EncouragementsComponent,
-    OpportunitiesComponent
+    OpportunitiesComponent,
+    FuseCalendarComponent
   ],
   imports: [
     RouterModule.forChild(routes),
+    RouterModule,
 
     MatTabsModule,
+    MatExpansionModule,
 
+    MatButtonModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatProgressBarModule,
+    MatDatepickerModule,
+    MatSlideToggleModule,
+    MatToolbarModule,
+
+    CalendarModule.forRoot(),
+    ColorPickerModule,
+
+    FuseProjectMembersModule,
+    FuseProjectTaskListModule,
+    FuseTruncatePipeModule,
     FuseSharedModule,
-    FuseCalendarModule,
-    FuseProjectsModule
+    FuseConfirmDialogModule,
+
+    NgbModalModule
   ]
 })
 
--- marketplace-frontend/src/app/main/content/engagements/opportunities/opportunities.component.ts
+++ marketplace-frontend/src/app/main/content/engagements/opportunities/opportunities.component.ts
@@ -1,5 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {fuseAnimations} from '../../../../../@fuse/animations';
+import {LoginService} from '../../_service/login.service';
+import {isArray, isNullOrUndefined} from 'util';
+import {ProjectService} from '../../_service/project.service';
+import {Project} from '../../_model/project';
+import {Subscription} from 'rxjs';
+import {Marketplace} from '../../_model/marketplace';
+import {CoreVolunteerService} from '../../_service/core-volunteer.service';
+import {MessageService} from '../../_service/message.service';
+import {Participant} from '../../_model/participant';
+import {Volunteer} from '../../_model/volunteer';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'fuse-opportunities',
@@ -8,11 +19,73 @@ import {fuseAnimations} from '../../../../../@fuse/animations';
   animations: fuseAnimations
 
 })
-export class OpportunitiesComponent implements OnInit {
+export class OpportunitiesComponent implements OnInit, OnDestroy {
+  private mySelf: boolean;
 
-  constructor() { }
+  public participant: Participant;
+  public registeredMarketplaces: Array<Marketplace> = [];
 
-  ngOnInit() {
+
+  public projects: Array<Project>;
+  private marketplaceChangeSubscription: Subscription;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private loginService: LoginService,
+              private messageService: MessageService,
+              private projectService: ProjectService,
+              private coreVolunteerService: CoreVolunteerService) {
   }
 
+  ngOnInit() {
+    this.loadProjects();
+    this.marketplaceChangeSubscription = this.messageService.subscribe('marketplaceSelectionChanged', this.loadProjects.bind(this));
+
+    this.route.params.subscribe((params) => {
+      const participantId = params['participantId'];
+      if (isNullOrUndefined(participantId)) {
+        this.mySelf = true;
+        this.loginService.getLoggedIn().toPromise().then((participant: Participant) => this.participant = participant);
+      } else {
+        this.mySelf = false;
+        this.coreVolunteerService.findById(participantId).toPromise().then((volunteer: Volunteer) => this.participant = volunteer);
+      }
+
+      this.coreVolunteerService.findRegisteredMarketplaces(this.participant.id).toPromise().then((registeredMarketplaces: Marketplace[]) => {
+        this.registeredMarketplaces = registeredMarketplaces;
+      });
+    });
+
+
+
+
+  }
+
+  ngOnDestroy() {
+    this.marketplaceChangeSubscription.unsubscribe();
+  }
+
+  private loadProjects() {
+    this.projects = new Array<Project>();
+    this.loginService.getLoggedIn().toPromise().then((volunteer: Participant) => {
+      const selected_marketplaces = JSON.parse(localStorage.getItem('marketplaces'));
+      if (!isArray(selected_marketplaces)) {
+        return;
+      }
+      this.coreVolunteerService.findRegisteredMarketplaces(volunteer.id)
+        .toPromise()
+        .then((marketplaces: Marketplace[]) => {
+          marketplaces
+            .filter(mp => selected_marketplaces.find(selected_mp => selected_mp.id === mp.id))
+            .forEach(marketplace => {
+              this.projectService.findEngaged(marketplace)
+                .toPromise().then((projects: Project[]) => this.projects = this.projects.concat(projects));
+            });
+        });
+    });
+  }
+
+  public calculateProgess(project: Project) {
+    return 50;
+  }
 }
