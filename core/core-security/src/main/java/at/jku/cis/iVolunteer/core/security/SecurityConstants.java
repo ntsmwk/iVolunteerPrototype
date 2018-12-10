68ad79661db71f5ca115f60583d961c9743d662c
Markus Weißenbek <markus.weissenbek@chatvisor.com>
2018-12-10 13:55:01 +0100

    redirect to login if token is expired

--- core/core-security/src/main/java/at/jku/cis/iVolunteer/core/security/SecurityConstants.java
+++ core/core-security/src/main/java/at/jku/cis/iVolunteer/core/security/SecurityConstants.java
@@ -3,6 +3,7 @@ package at.jku.cis.iVolunteer.core.security;
 public interface SecurityConstants {
 	public static final String SECRET = "SecretKeyToGenJWTs";
 	public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+	
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
 	
--- marketplace-frontend/src/app/main/content/_guard/token.guard.ts
+++ marketplace-frontend/src/app/main/content/_guard/token.guard.ts
@@ -1,18 +1,22 @@
 import {Injectable} from '@angular/core';
 import {CanActivate, Router} from '@angular/router';
 import {isNullOrUndefined} from 'util';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenGuard implements CanActivate {
 
+  private jwtHelper:JwtHelperService  = new JwtHelperService();
+
   constructor(private router: Router) {
   }
 
   canActivate(): boolean {
     const token = localStorage.getItem('token');
-    if (isNullOrUndefined(token) || token.length === 0) {
+
+    if (isNullOrUndefined(token) || token.length === 0 || this.jwtHelper.isTokenExpired(token)) {
       this.router.navigate(['login']);
       return false;
     }
