b0e0d0ca083d72e92169fbf3d9e8eadd15f6ed68
ntsmwk <markus.weissenbek@gmx.at>
2019-12-06 08:54:04 +0100

    fixed initialization service

---
+++ marketplace/marketplace-model/src/main/java/at/jku/cis/iVolunteer/model/rule/DerivationRule.java
@@ -0,0 +1,12 @@
+package at.jku.cis.iVolunteer.model.rule;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class DerivationRule {
+
+//	TODO @MWE
+	
+}
---
+++ marketplace/marketplace-model/src/main/java/at/jku/cis/iVolunteer/model/rule/MappingOperatorType.java
@@ -0,0 +1,6 @@
+package at.jku.cis.iVolunteer.model.rule;
+
+public enum MappingOperatorType {
+
+	COUNT
+}
