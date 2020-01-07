2074443a60a985277165235fe6fb15aae657cfef
alexander <alexander.kopp@gmx.at>
2020-01-07 12:56:41 +0100

    created inbox>confirm flow, added dummy success screen

--- marketplace-frontend/src/app/main/content/asset-inbox-volunteer/asset-inbox-volunteer.component.ts
+++ marketplace-frontend/src/app/main/content/asset-inbox-volunteer/asset-inbox-volunteer.component.ts
@@ -25,6 +25,8 @@ export class AssetInboxVolunteerComponent implements OnInit {
   participant: Participant;
   classInstances: ClassInstance[];
 
+  submitPressed: boolean;
+
 
   constructor(
     private route: ActivatedRoute,
@@ -48,14 +50,15 @@ export class AssetInboxVolunteerComponent implements OnInit {
     console.log(this.participant);
 
     if (!isNullOrUndefined(this.marketplace) && !isNullOrUndefined(this.participant)) {
-      console.log("if branch");
+      console.log('if branch');
       this.loadInboxEntries();
     } else {
-      console.log("else branch");
+      console.log('else branch');
       Promise.all([
         this.marketplaceService.findAll().toPromise().then((marketplaces: Marketplace[]) => {
-          if (!isNullOrUndefined(marketplaces))
-            this.marketplace = marketplaces[0]
+          if (!isNullOrUndefined(marketplaces)) {
+            this.marketplace = marketplaces[0];
+          }
         }),
         this.loginService.getLoggedIn().toPromise().then((participant: Participant) => {
           this.participant = participant;
@@ -77,6 +80,12 @@ export class AssetInboxVolunteerComponent implements OnInit {
   }
 
   close() {
+  }
+
+  onAssetInboxSubmit(event: ClassInstance[]) {
+    console.log('submitted - returned');
+    console.log(event);
+    this.router.navigate(['/main/volunteer/asset-inbox/confirm']);
   }
 
 
