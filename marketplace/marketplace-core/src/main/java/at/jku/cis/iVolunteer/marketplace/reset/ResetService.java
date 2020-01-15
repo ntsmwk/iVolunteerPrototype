56752f680844b55b0ec1f8cb15c5699d4564ff93
ntsmwk <markus.weissenbek@gmx.at>
2020-01-15 12:25:17 +0100

    asdf

--- marketplace/marketplace-core/src/main/java/at/jku/cis/iVolunteer/marketplace/reset/ResetService.java
+++ marketplace/marketplace-core/src/main/java/at/jku/cis/iVolunteer/marketplace/reset/ResetService.java
@@ -1,12 +1,34 @@
 package at.jku.cis.iVolunteer.marketplace.reset;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import at.jku.cis.iVolunteer.marketplace.rule.DerivationRuleRepository;
 
 @Service
 public class ResetService {
 
-	
+	@Autowired private DerivationRuleRepository derivationRuleRepository;
+
 	public void reset() {
+
+		resetFahrtenspangeFake();
+
+//		Reset Sunburst Fake
+			//Reset ClassDefinitions
+//			//Reset Flag for retrieval
 		
+//		Reset Sybos Fake
+		
+//		Reset
+	}
+
+	private void resetFahrtenspangeFake() {
+		// Reset Fahrtenspange Derivation-Rule
+		derivationRuleRepository.deleteAll();
+
+		// Reset Fahrtenspange Task
+
+		// Reset Fahrtenspange Badge
 	}
 }
