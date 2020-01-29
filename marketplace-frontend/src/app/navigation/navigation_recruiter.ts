ba5567d62e37145b048e98f1143071771aa00efe
ntsmwk <markus.weissenbek@gmx.at>
2020-01-29 14:58:17 +0100

    ..

--- marketplace-frontend/src/app/navigation/navigation_recruiter.ts
+++ marketplace-frontend/src/app/navigation/navigation_recruiter.ts
@@ -1,5 +1,12 @@
 export const navigation_recruiter = [
   {
+    'id': 'jobs',
+    'title': 'Jobangebote',
+    'type': 'item',
+    'icon': 'dashboard',
+    // 'url': '/main/recruitment'
+  },
+  {
     'id': 'recruitment',
     'title': 'Bewerbungen',
     'type': 'item',
