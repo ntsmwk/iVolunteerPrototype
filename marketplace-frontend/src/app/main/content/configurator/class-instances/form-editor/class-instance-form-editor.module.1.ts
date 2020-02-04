d356df20750b34f95e2db86c84089f6d7cd70840
alexander <alexander.kopp@gmx.at>
2020-02-04 16:14:24 +0100

    ...

---
+++ marketplace-frontend/src/app/main/content/configurator/class-instances/form-editor/class-instance-form-editor.module.1.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClassInstanceFormEditorComponent } from './class-instance-form-editor.component';
+import { RouterModule } from '@angular/router';
+import { MatCommonModule, MatProgressSpinnerModule, MatIconModule, MatTableModule, MatExpansionModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatOptionModule, MatCardModule, MatSlideToggleModule, MatDatepicker, MatDatepickerModule, MatDividerModule } from '@angular/material';
+import { FuseTruncatePipeModule } from '../../../_pipe/truncate-pipe.module';
+import { FuseSharedModule } from '@fuse/shared.module';
+import { DynamicClassInstanceCreationFormModule } from 'app/main/content/_components/dynamic-forms/dynamic-class-instance-creation-form/dynamic-class-instance-creation-form.module';
+
+const routes = [
+  {path: ':marketplaceId/:classId/:showMaxGluehtemperatur', component: ClassInstanceFormEditorComponent}
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+
+    
+    MatCommonModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatTableModule,
+    MatExpansionModule,
+
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatCardModule,
+    MatSlideToggleModule,
+    MatDatepickerModule,
+    MatDividerModule,
+    MatIconModule,
+
+
+    FuseSharedModule,
+    FuseTruncatePipeModule,
+
+    DynamicClassInstanceCreationFormModule,
+
+  ],
+  declarations: [ClassInstanceFormEditorComponent],
+  // exports: [ClassInstanceFormEditorComponent]
+
+})
+
+
+
+export class ClassInstanceFormEditorModule { }
