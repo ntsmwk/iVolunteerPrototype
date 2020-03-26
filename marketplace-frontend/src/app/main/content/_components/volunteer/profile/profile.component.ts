a7dda7d2d27e22a9be4b476171515be010e37a93
ntsmwk <markus.weissenbek@gmx.at>
2020-03-26 11:46:34 +0100

    added volunteer profile

---
+++ marketplace-frontend/src/app/main/content/_components/volunteer/profile/profile.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "volunteer-profile",
+  templateUrl: "profile.component.html",
+  styleUrls: ["profile.component.scss"]
+})
+export class VolunteerProfileComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ marketplace-frontend/src/app/main/content/_components/volunteer/profile/profile.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+
+import { VolunteerProfileComponent } from "./profile.component";
+import { RouterLink, RouterState, RouterModule } from "@angular/router";
+
+const routes = [{ path: "", component: VolunteerProfileComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [],
+  declarations: [VolunteerProfileComponent],
+  providers: []
+})
+export class VolunteeProfileModule {}
