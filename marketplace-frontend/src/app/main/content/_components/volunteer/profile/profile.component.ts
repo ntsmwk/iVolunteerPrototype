882f0d5351b58780ac07465fdeaea195735374a2
ntsmwk <markus.weissenbek@gmx.at>
2020-03-27 08:51:16 +0100

    ..

--- marketplace-frontend/src/app/main/content/_components/volunteer/profile/profile.component.ts
+++ marketplace-frontend/src/app/main/content/_components/volunteer/profile/profile.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from "@angular/core";
+import { fuseAnimations } from "@fuse/animations";
 
 @Component({
   selector: "volunteer-profile",
   templateUrl: "profile.component.html",
-  styleUrls: ["profile.component.scss"]
+  styleUrls: ["profile.component.scss"],
+  animations: fuseAnimations
 })
 export class VolunteerProfileComponent implements OnInit {
   constructor() {}
