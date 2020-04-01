d7943f706b031fa23a0d76566e028a5f89beb90a
ntsmwk <markus.weissenbek@gmx.at>
2020-04-01 10:00:29 +0200

    ..

--- marketplace-frontend/src/app/main/content/_shared_components/organisation-filter/organisation-filter.component.ts
+++ marketplace-frontend/src/app/main/content/_shared_components/organisation-filter/organisation-filter.component.ts
@@ -1,11 +1,39 @@
 import { Component, OnInit } from "@angular/core";
+import { CoreVolunteerService } from "../../_service/core-volunteer.service";
+import { CoreHelpSeekerService } from "../../_service/core-helpseeker.service";
+import { LoginService } from "../../_service/login.service";
+import { CoreMarketplaceService } from "../../_service/core-marketplace.service";
+import { TenantService } from "../../_service/core-tenant.service";
+import { Volunteer } from "../../_model/volunteer";
+import { Participant } from "../../_model/participant";
+import { Marketplace } from "../../_model/marketplace";
 
 @Component({
   selector: "organisation-filter",
   templateUrl: "organisation-filter.component.html"
 })
 export class OrganisationFilterComponent implements OnInit {
-  constructor() {}
+  participant: Participant;
+  marketplace: Marketplace;
 
-  ngOnInit() {}
+  constructor(
+    private coreVolunteerService: CoreVolunteerService,
+    private coreHelpseekerService: CoreHelpSeekerService,
+    private loginService: LoginService,
+    private marketplaceService: CoreMarketplaceService,
+    private tenantService: TenantService
+  ) {}
+
+  async ngOnInit() {
+    this.participant = <Participant>(
+      await this.loginService.getLoggedIn().toPromise()
+    );
+    // this.setVolunteerImage();
+
+    this.marketplace = (<Marketplace[]>(
+      await this.coreVolunteerService
+        .findRegisteredMarketplaces(this.part.id)
+        .toPromise()
+    )).filter(m => (m.name = "Marketplace 1"))[0];
+  }
 }
