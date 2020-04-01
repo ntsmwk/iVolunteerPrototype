b3f3424649c2ad67d4360f06ef71f24089b8ddad
ntsmwk <markus.weissenbek@gmx.at>
2020-04-01 08:50:05 +0200

    renamed organisation-filter organisation

---
+++ marketplace-frontend/src/app/main/content/_shared_components/organisation-filter/organisation-filter.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "organisation-filter",
+  templateUrl: "organisation-filter.component.html"
+})
+export class OrganisationFilterComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ marketplace-frontend/src/app/main/content/_shared_components/organisation-filter/organisation-filter.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+
+import { OrganisationFilterComponent } from "./organisation-filter.component";
+
+@NgModule({
+  imports: [],
+  exports: [OrganisationFilterComponent],
+  declarations: [OrganisationFilterComponent],
+  providers: []
+})
+export class OrganisationFilterModule {}
