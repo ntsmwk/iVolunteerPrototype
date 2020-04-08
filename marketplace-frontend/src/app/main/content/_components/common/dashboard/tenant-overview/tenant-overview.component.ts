0209856e4846a1152f5dc73f8280269076831c3b
ntsmwk <markus.weissenbek@gmx.at>
2020-04-08 10:47:00 +0200

    tenant overview finished

--- marketplace-frontend/src/app/main/content/_components/common/dashboard/tenant-overview/tenant-overview.component.ts
+++ marketplace-frontend/src/app/main/content/_components/common/dashboard/tenant-overview/tenant-overview.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { TenantService } from "app/main/content/_service/core-tenant.service";
 import { Tenant } from "app/main/content/_model/tenant";
 import { ImageService } from "app/main/content/_service/image.service";
+import { LoginService } from "app/main/content/_service/login.service";
+import { Volunteer } from "app/main/content/_model/volunteer";
 
 @Component({
   selector: "tenant-overview",
@@ -10,17 +12,28 @@ import { ImageService } from "app/main/content/_service/image.service";
 })
 export class TenantOverviewComponent implements OnInit {
   tenants: Tenant[] = [];
+  volunteer: Volunteer;
 
   constructor(
+    private loginService: LoginService,
     private tenantService: TenantService,
     private imageService: ImageService
   ) {}
 
   async ngOnInit() {
+    this.volunteer = <Volunteer>(
+      await this.loginService.getLoggedIn().toPromise()
+    );
     this.tenants = <Tenant[]>await this.tenantService.findAll().toPromise();
   }
 
   getTenantImage(tenant: Tenant) {
     return this.imageService.getImgSourceFromBytes(tenant.image);
+  }
+
+  isSubscribed(tenant: Tenant) {
+    return (
+      this.volunteer.subscribedTenants.findIndex((t) => t === tenant.id) >= 0
+    );
   }
 }
