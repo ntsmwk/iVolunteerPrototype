270d1d4c3874139bd29bd967339e1848179773c8
ntsmwk <markus.weissenbek@gmx.at>
2020-04-14 14:28:40 +0200

    -.

--- marketplace-frontend/src/app/main/content/_components/common/profile/volunteer-profile/volunteer-profile.component.ts
+++ marketplace-frontend/src/app/main/content/_components/common/profile/volunteer-profile/volunteer-profile.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from "@angular/core";
+import { Participant } from "app/main/content/_model/participant";
+import { LoginService } from "app/main/content/_service/login.service";
 
 @Component({
   selector: "volunteer-profile",
   templateUrl: "volunteer-profile.component.html"
 })
 export class VolunteerProfileComponent implements OnInit {
-  constructor() {}
+  volunteer: Participant;
 
-  ngOnInit() {}
+  constructor(private loginService: LoginService) {}
+
+  async ngOnInit() {
+    this.volunteer = <Participant>(
+      await this.loginService.getLoggedIn().toPromise()
+    );
+  }
 }
