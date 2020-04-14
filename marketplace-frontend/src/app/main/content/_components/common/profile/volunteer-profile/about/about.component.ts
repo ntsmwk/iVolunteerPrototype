2d1447027687920a9e897d1b24aa3bdb2971662e
ntsmwk <markus.weissenbek@gmx.at>
2020-04-14 12:00:41 +0200

    removed unnecessary

--- marketplace-frontend/src/app/main/content/_components/common/profile/volunteer-profile/about/about.component.ts
+++ marketplace-frontend/src/app/main/content/_components/common/profile/volunteer-profile/about/about.component.ts
@@ -1,60 +1,38 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from "@angular/core";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
-import { fuseAnimations } from '@fuse/animations';
-import { ProfileService } from 'app/main/pages/profile/profile.service';
+import { fuseAnimations } from "@fuse/animations";
 
 @Component({
-    selector     : 'profile-about',
-    templateUrl  : './about.component.html',
-    styleUrls    : ['./about.component.scss'],
-    encapsulation: ViewEncapsulation.None,
-    animations   : fuseAnimations
+  selector: "profile-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.scss"],
+  encapsulation: ViewEncapsulation.None,
+  animations: fuseAnimations
 })
-export class ProfileAboutComponent implements OnInit, OnDestroy
-{
-    about: any;
+export class ProfileAboutComponent implements OnInit {
+  about: any;
 
-    // Private
-    private _unsubscribeAll: Subject<any>;
+  // Private
+  private _unsubscribeAll: Subject<any>;
 
-    /**
-     * Constructor
-     *
-     * @param {ProfileService} _profileService
-     */
-    constructor(
-        private _profileService: ProfileService
-    )
-    {
-        // Set the private defaults
-        this._unsubscribeAll = new Subject();
-    }
+  /**
+   * Constructor
+   *
+   * @param {ProfileService} _profileService
+   */
+  constructor() {
+    // Set the private defaults
+    this._unsubscribeAll = new Subject();
+  }
 
-    // -----------------------------------------------------------------------------------------------------
-    // @ Lifecycle hooks
-    // -----------------------------------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------------------------------
+  // @ Lifecycle hooks
+  // -----------------------------------------------------------------------------------------------------
 
-    /**
-     * On init
-     */
-    ngOnInit(): void
-    {
-        this._profileService.aboutOnChanged
-            .pipe(takeUntil(this._unsubscribeAll))
-            .subscribe(about => {
-                this.about = about;
-            });
-    }
-
-    /**
-     * On destroy
-     */
-    ngOnDestroy(): void
-    {
-        // Unsubscribe from all subscriptions
-        this._unsubscribeAll.next();
-        this._unsubscribeAll.complete();
-    }
+  /**
+   * On init
+   */
+  ngOnInit(): void {}
 }
--- marketplace-frontend/src/app/main/content/_components/common/profile/volunteer-profile/timeline/timeline.component.ts
+++ marketplace-frontend/src/app/main/content/_components/common/profile/volunteer-profile/timeline/timeline.component.ts
@@ -1,61 +1,18 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from "@angular/core";
 
-import { fuseAnimations } from '@fuse/animations';
-
-import { ProfileService } from '../../profile.service';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { fuseAnimations } from "@fuse/animations";
 
 @Component({
-    selector     : 'profile-timeline',
-    templateUrl  : './timeline.component.html',
-    styleUrls    : ['./timeline.component.scss'],
-    encapsulation: ViewEncapsulation.None,
-    animations   : fuseAnimations
+  selector: "profile-timeline",
+  templateUrl: "./timeline.component.html",
+  styleUrls: ["./timeline.component.scss"],
+  encapsulation: ViewEncapsulation.None,
+  animations: fuseAnimations
 })
-export class ProfileTimelineComponent implements OnInit, OnDestroy
-{
-    timeline: any;
+export class ProfileTimelineComponent implements OnInit {
+  timeline: any;
 
-    // Private
-    private _unsubscribeAll: Subject<any>;
+  constructor() {}
 
-    /**
-     * Constructor
-     *
-     * @param {ProfileService} _profileService
-     */
-    constructor(
-        private _profileService: ProfileService
-    )
-    {
-        // Set the private defaults
-        this._unsubscribeAll = new Subject();
-    }
-
-    // -----------------------------------------------------------------------------------------------------
-    // @ Lifecycle hooks
-    // -----------------------------------------------------------------------------------------------------
-
-    /**
-     * On init
-     */
-    ngOnInit(): void
-    {
-        this._profileService.timelineOnChanged
-            .pipe(takeUntil(this._unsubscribeAll))
-            .subscribe(timeline => {
-                this.timeline = timeline;
-            });
-    }
-
-    /**
-     * On destroy
-     */
-    ngOnDestroy(): void
-    {
-        // Unsubscribe from all subscriptions
-        this._unsubscribeAll.next();
-        this._unsubscribeAll.complete();
-    }
+  ngOnInit(): void {}
 }
