81051ba3fc6db43525ec08637ee070b3277ba244
ntsmwk <markus.weissenbek@gmx.at>
2020-04-15 11:10:45 +0200

    ..

--- marketplace-frontend/src/app/main/content/_components/common/profile/volunteer-profile/timeline/timeline.component.ts
+++ marketplace-frontend/src/app/main/content/_components/common/profile/volunteer-profile/timeline/timeline.component.ts
@@ -2,6 +2,10 @@ import { Component, OnDestroy, OnInit, ViewEncapsulation } from "@angular/core";
 
 import { fuseAnimations } from "@fuse/animations";
 
+import { ProfileService } from "../../profile.service";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+
 @Component({
   selector: "profile-timeline",
   templateUrl: "./timeline.component.html",
@@ -9,10 +13,43 @@ import { fuseAnimations } from "@fuse/animations";
   encapsulation: ViewEncapsulation.None,
   animations: fuseAnimations
 })
-export class ProfileTimelineComponent implements OnInit {
+export class ProfileTimelineComponent implements OnInit, OnDestroy {
   timeline: any;
 
-  constructor() {}
+  // Private
+  private _unsubscribeAll: Subject<any>;
 
-  ngOnInit(): void {}
+  /**
+   * Constructor
+   *
+   * @param {ProfileService} _profileService
+   */
+  constructor(private _profileService: ProfileService) {
+    // Set the private defaults
+    this._unsubscribeAll = new Subject();
+  }
+
+  // -----------------------------------------------------------------------------------------------------
+  // @ Lifecycle hooks
+  // -----------------------------------------------------------------------------------------------------
+
+  /**
+   * On init
+   */
+  ngOnInit(): void {
+    this._profileService.timelineOnChanged
+      .pipe(takeUntil(this._unsubscribeAll))
+      .subscribe(timeline => {
+        this.timeline = timeline;
+      });
+  }
+
+  /**
+   * On destroy
+   */
+  ngOnDestroy(): void {
+    // Unsubscribe from all subscriptions
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
+  }
 }
