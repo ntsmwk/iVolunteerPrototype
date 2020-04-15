aef8e6038d38035f8852d706cad60721d6813e52
ntsmwk <markus.weissenbek@gmx.at>
2020-04-15 09:52:01 +0200

    ..

--- marketplace-frontend/src/app/app.component.ts
+++ marketplace-frontend/src/app/app.component.ts
@@ -1,35 +1,33 @@
-import {Component} from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';
+import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
-import {FuseSplashScreenService} from '@fuse/services/splash-screen.service';
-import {FuseTranslationLoaderService} from '@fuse/services/translation-loader.service';
-import {FuseNavigationService} from '@fuse/components/navigation/navigation.service';
+import { FuseSplashScreenService } from "@fuse/services/splash-screen.service";
+import { FuseTranslationLoaderService } from "@fuse/services/translation-loader.service";
+import { FuseNavigationService } from "@fuse/components/navigation/navigation.service";
 
 @Component({
-    selector   : 'fuse-root',
-    templateUrl: './app.component.html',
-    styleUrls  : ['./app.component.scss']
+  selector: "fuse-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent
-{
-    constructor(
-        private translate: TranslateService,
-        private fuseNavigationService: FuseNavigationService,
-        private fuseSplashScreen: FuseSplashScreenService,
-        private fuseTranslationLoader: FuseTranslationLoaderService
-    )
-    {
-        // Add languages
-        // this.translate.addLangs(['en', 'tr']);
+export class AppComponent {
+  constructor(
+    private translate: TranslateService,
+    private fuseNavigationService: FuseNavigationService,
+    private fuseSplashScreen: FuseSplashScreenService,
+    private fuseTranslationLoader: FuseTranslationLoaderService
+  ) {
+    // Add languages
+    // this.translate.addLangs(['en', 'tr']);
 
-        // Set the default language
-        // this.translate.setDefaultLang('en');
+    // Set the default language
+    // this.translate.setDefaultLang('en');
 
-        // Set the navigation translations
-        // this.fuseTranslationLoader.loadTranslations(navigationEnglish, navigationTurkish);
+    // Set the navigation translations
+    // this.fuseTranslationLoader.loadTranslations(navigationEnglish, navigationTurkish);
 
-        // Use a language
-        // this.translate.use('en');
-        this.fuseSplashScreen.hide();
-    }
+    // Use a language
+    // this.translate.use('en');
+    this.fuseSplashScreen.hide();
+  }
 }
--- marketplace-frontend/src/app/main/content/_components/common/profile/volunteer-profile/volunteer-profile.component.ts
+++ marketplace-frontend/src/app/main/content/_components/common/profile/volunteer-profile/volunteer-profile.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from "@angular/core";
 import { Participant } from "app/main/content/_model/participant";
 import { LoginService } from "app/main/content/_service/login.service";
+import { fuseAnimations } from "@fuse/animations";
+import { ImageService } from "app/main/content/_service/image.service";
 
 @Component({
   selector: "volunteer-profile",
-  templateUrl: "volunteer-profile.component.html"
+  templateUrl: "volunteer-profile.component.html",
+  styleUrls: ["volunteer-profile.component.scss"],
+  animations: fuseAnimations
 })
 export class VolunteerProfileComponent implements OnInit {
   volunteer: Participant;
 
-  constructor(private loginService: LoginService) {}
+  constructor(
+    private loginService: LoginService,
+    private imageService: ImageService
+  ) {}
 
   async ngOnInit() {
     this.volunteer = <Participant>(
       await this.loginService.getLoggedIn().toPromise()
     );
+    console.error(this.volunteer);
+  }
+
+  getProfileImage() {
+    return this.imageService.getImgSourceFromBytes(this.volunteer.image);
   }
 }
