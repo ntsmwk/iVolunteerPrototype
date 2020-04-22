985ab5d7b134991789fd7657577440f1ff6524dd
ntsmwk <markus.weissenbek@gmx.at>
2020-04-22 13:47:31 +0200

    ..

--- marketplace-frontend/src/app/main/content/_service/local-repository.service.ts
+++ marketplace-frontend/src/app/main/content/_service/local-repository.service.ts
@@ -1,235 +1,249 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Volunteer } from '../_model/volunteer';
-import { LocalRepository } from '../_model/local-repository';
-import { isNullOrUndefined } from 'util';
-import { ClassInstanceDTO, ClassInstance } from '../_model/meta/Class';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Volunteer } from "../_model/volunteer";
+import { LocalRepository } from "../_model/local-repository";
+import { isNullOrUndefined } from "util";
+import { ClassInstanceDTO, ClassInstance } from "../_model/meta/Class";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root"
 })
 export class LocalRepositoryService {
+  // private apiUrl = 'http://localhost:3000/repository';
+  private apiUrl = "http://140.78.92.57:3000/repository";
 
-    private apiUrl = 'http://localhost:3000/repository';
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient) {
+  async isConnected(volunteer: Volunteer) {
+    let isConnected;
+
+    let localRepos = <LocalRepository[]>await this.http
+      .get(this.apiUrl)
+      .toPromise()
+      .catch(() => (isConnected = false));
+
+    if (localRepos) {
+      isConnected = true;
+      if (localRepos.findIndex(l => l.id === volunteer.id) === -1) {
+        let newRepo = new LocalRepository(volunteer.id, volunteer.username);
+        this.http
+          .post(this.apiUrl, newRepo)
+          .toPromise()
+          .catch(e => console.log(e));
+      }
     }
 
+    return isConnected;
+  }
 
-    async isConnected(volunteer: Volunteer) {
-        let isConnected;
+  findByVolunteer(volunteer: Volunteer) {
+    const observable = new Observable(subscriber => {
+      const successFunction = (localRepository: LocalRepository) => {
+        subscriber.next(localRepository);
+        subscriber.complete();
+      };
 
-        let localRepos = <LocalRepository[]>await this.http.get(this.apiUrl).toPromise().catch(() => isConnected = false);
+      const failureFunction = (error: any) => {
+        subscriber.error(error);
+        subscriber.complete();
+      };
 
-        if (localRepos) {
-            isConnected = true;
-            if (localRepos.findIndex(l => l.id === volunteer.id) === -1) {
-                let newRepo = new LocalRepository(volunteer.id, volunteer.username);
-                this.http.post(this.apiUrl, newRepo).toPromise().catch(e => console.log(e));
-            }
+      this.http
+        .get(this.apiUrl)
+        .toPromise()
+        .then((localRepositorys: LocalRepository[]) => {
+          successFunction(
+            localRepositorys.find((localRepository: LocalRepository) => {
+              return localRepository.volunteerUsername === volunteer.username;
+            })
+          );
+        })
+        .catch((error: any) => failureFunction(error));
+    });
+    return observable;
+  }
+
+  findClassInstancesByVolunteer(volunteer: Volunteer) {
+    const observable = new Observable(subscriber => {
+      const failureFunction = (error: any) => {
+        subscriber.error(error);
+        subscriber.complete();
+      };
+
+      const successFunction = (localRepository: LocalRepository) => {
+        if (isNullOrUndefined(localRepository)) {
+          subscriber.next([]);
+        } else {
+          subscriber.next(localRepository.classInstances);
+          subscriber.complete();
         }
+      };
 
-        return isConnected;
+      this.findByVolunteer(volunteer)
+        .toPromise()
+        .then((localRepository: LocalRepository) =>
+          successFunction(localRepository)
+        )
+        .catch((error: any) => failureFunction(error));
+    });
 
-    }
+    return observable;
+  }
 
-    findByVolunteer(volunteer: Volunteer) {
-        const observable = new Observable(subscriber => {
+  synchronizeSingleClassInstance(
+    volunteer: Volunteer,
+    classInstance: ClassInstanceDTO
+  ) {
+    const observable = new Observable(subscriber => {
+      const failureFunction = (error: any) => {
+        subscriber.error(error);
+        subscriber.complete();
+      };
 
-            const successFunction = (localRepository: LocalRepository) => {
-                subscriber.next(localRepository);
-                subscriber.complete();
-            };
+      this.findByVolunteer(volunteer)
+        .toPromise()
+        .then((localRepository: LocalRepository) => {
+          localRepository.classInstances.push(classInstance);
 
-            const failureFunction = (error: any) => {
-                subscriber.error(error);
-                subscriber.complete();
-            };
+          this.http
+            .put(`${this.apiUrl}/${localRepository.id}`, localRepository)
+            .toPromise()
+            .then(() => subscriber.complete())
+            .catch((error: any) => failureFunction(error));
 
-            this.http.get(this.apiUrl)
-                .toPromise()
-                .then((localRepositorys: LocalRepository[]) => {
-                    successFunction(localRepositorys.find((localRepository: LocalRepository) => {
-                        return localRepository.volunteerUsername === volunteer.username;
-                    }));
-                })
-                .catch((error: any) => failureFunction(error));
-        });
-        return observable;
-    }
+          subscriber.next(localRepository.classInstances);
+        })
+        .catch((error: any) => failureFunction(error));
+    });
 
-    findClassInstancesByVolunteer(volunteer: Volunteer) {
-        const observable = new Observable(subscriber => {
-            const failureFunction = (error: any) => {
-                subscriber.error(error);
-                subscriber.complete();
-            };
+    return observable;
+  }
 
-            const successFunction = (localRepository: LocalRepository) => {
-                if (isNullOrUndefined(localRepository)) {
-                    subscriber.next([]);
-                } else {
-                    subscriber.next(localRepository.classInstances);
-                    subscriber.complete();
-                }
-            };
+  synchronizeClassInstances(
+    volunteer: Volunteer,
+    classInstances: ClassInstanceDTO[]
+  ) {
+    const observable = new Observable(subscriber => {
+      const failureFunction = (error: any) => {
+        subscriber.error(error);
+        subscriber.complete();
+      };
 
-            this.findByVolunteer(volunteer)
-                .toPromise()
-                .then((localRepository: LocalRepository) => successFunction(localRepository))
-                .catch((error: any) => failureFunction(error));
-        });
+      this.findByVolunteer(volunteer)
+        .toPromise()
+        .then((localRepository: LocalRepository) => {
+          // TODO: prevent double insertion
+          localRepository.classInstances = [
+            ...localRepository.classInstances,
+            ...classInstances
+          ];
 
-        return observable;
-    }
+          this.http
+            .put(`${this.apiUrl}/${localRepository.id}`, localRepository)
+            .toPromise()
+            .then(() => subscriber.complete())
+            .catch((error: any) => failureFunction(error));
 
+          subscriber.next(localRepository.classInstances);
+        })
+        .catch((error: any) => failureFunction(error));
+    });
 
-    synchronizeSingleClassInstance(volunteer: Volunteer, classInstance: ClassInstanceDTO) {
-        const observable = new Observable(subscriber => {
-            const failureFunction = (error: any) => {
-                subscriber.error(error);
-                subscriber.complete();
-            };
+    return observable;
+  }
 
-            this.findByVolunteer(volunteer)
-                .toPromise()
-                .then((localRepository: LocalRepository) => {
-                    localRepository.classInstances.push(classInstance);
+  removeSingleClassInstance(
+    volunteer: Volunteer,
+    classInstance: ClassInstanceDTO
+  ) {
+    const observable = new Observable(subscriber => {
+      const failureFunction = (error: any) => {
+        subscriber.error(error);
+        subscriber.complete();
+      };
 
-                    this.http.put(`${this.apiUrl}/${localRepository.id}`, localRepository)
-                        .toPromise()
-                        .then(() => subscriber.complete())
-                        .catch((error: any) => failureFunction(error));
+      this.findByVolunteer(volunteer)
+        .toPromise()
+        .then((localRepository: LocalRepository) => {
+          localRepository.classInstances.forEach((ci, index, object) => {
+            if (ci.id === classInstance.id) {
+              object.splice(index, 1);
+            }
+          });
 
-                    subscriber.next(localRepository.classInstances);
+          this.http
+            .put(`${this.apiUrl}/${localRepository.id}`, localRepository)
+            .toPromise()
+            .then(() => subscriber.complete())
+            .catch((error: any) => failureFunction(error));
 
-                })
-                .catch((error: any) => failureFunction(error));
-        });
+          subscriber.next(localRepository.classInstances);
+        })
+        .catch((error: any) => failureFunction(error));
+    });
 
-        return observable;
-    }
+    return observable;
+  }
 
-    synchronizeClassInstances(volunteer: Volunteer, classInstances: ClassInstanceDTO[]) {
-        const observable = new Observable(subscriber => {
-            const failureFunction = (error: any) => {
-                subscriber.error(error);
-                subscriber.complete();
-            };
+  // TODO: workaround for removeClassInstances
+  setClassInstances(volunteer: Volunteer, classInstances: ClassInstanceDTO[]) {
+    const observable = new Observable(subscriber => {
+      const failureFunction = (error: any) => {
+        subscriber.error(error);
+        subscriber.complete();
+      };
 
-            this.findByVolunteer(volunteer)
-                .toPromise()
-                .then((localRepository: LocalRepository) => {
-                    // TODO: prevent double insertion
-                    localRepository.classInstances = [...localRepository.classInstances, ...classInstances];
+      this.findByVolunteer(volunteer)
+        .toPromise()
+        .then((localRepository: LocalRepository) => {
+          localRepository.classInstances = classInstances;
 
-                    this.http.put(`${this.apiUrl}/${localRepository.id}`, localRepository)
-                        .toPromise()
-                        .then(() => subscriber.complete())
-                        .catch((error: any) => failureFunction(error));
+          this.http
+            .put(`${this.apiUrl}/${localRepository.id}`, localRepository)
+            .toPromise()
+            .then(() => subscriber.complete())
+            .catch((error: any) => failureFunction(error));
 
-                    subscriber.next(localRepository.classInstances);
+          subscriber.next(localRepository.classInstances);
+        })
+        .catch((error: any) => failureFunction(error));
+    });
 
-                })
-                .catch((error: any) => failureFunction(error));
-        });
+    return observable;
+  }
 
-        return observable;
-    }
+  // TODO: not working as intended...
+  // removeClassInstances(volunteer: Volunteer, classInstances: ClassInstanceDTO[]) {
+  //     const observable = new Observable(subscriber => {
+  //         const failureFunction = (error: any) => {
+  //             subscriber.error(error);
+  //             subscriber.complete();
+  //         };
 
+  //         this.findByVolunteer(volunteer)
+  //             .toPromise()
+  //             .then((localRepository: LocalRepository) => {
 
-    removeSingleClassInstance(volunteer: Volunteer, classInstance: ClassInstanceDTO) {
-        const observable = new Observable(subscriber => {
-            const failureFunction = (error: any) => {
-                subscriber.error(error);
-                subscriber.complete();
-            };
+  //                 // localRepository.classInstances.forEach((ci, index, object) => {
+  //                 //     classInstances.forEach(t => {
+  //                 //         if (ci.id === t.id) {
+  //                 //             object.splice(index, 1);
+  //                 //         }
+  //                 //     });
+  //                 // });
 
-            this.findByVolunteer(volunteer)
-                .toPromise()
-                .then((localRepository: LocalRepository) => {
-                    localRepository.classInstances.forEach((ci, index, object) => {
-                        if (ci.id === classInstance.id) {
-                            object.splice(index, 1);
-                        }
-                    });
+  //                 this.http.put(`${this.apiUrl}/${localRepository.id}`, localRepository)
+  //                     .toPromise()
+  //                     .then(() => subscriber.complete())
+  //                     .catch((error: any) => failureFunction(error));
 
-                    this.http.put(`${this.apiUrl}/${localRepository.id}`, localRepository)
-                        .toPromise()
-                        .then(() => subscriber.complete())
-                        .catch((error: any) => failureFunction(error));
+  //                 subscriber.next(localRepository.classInstances);
 
-                    subscriber.next(localRepository.classInstances);
+  //             })
+  //             .catch((error: any) => failureFunction(error));
+  //     });
 
-                })
-                .catch((error: any) => failureFunction(error));
-        });
-
-        return observable;
-    }
-
-        // TODO: workaround for removeClassInstances
-        setClassInstances(volunteer: Volunteer, classInstances: ClassInstanceDTO[]) {
-        const observable = new Observable(subscriber => {
-            const failureFunction = (error: any) => {
-                subscriber.error(error);
-                subscriber.complete();
-            };
-
-            this.findByVolunteer(volunteer)
-                .toPromise()
-                .then((localRepository: LocalRepository) => {
-                  
-                    localRepository.classInstances = classInstances;
-                   
-                    this.http.put(`${this.apiUrl}/${localRepository.id}`, localRepository)
-                        .toPromise()
-                        .then(() => subscriber.complete())
-                        .catch((error: any) => failureFunction(error));
-
-                    subscriber.next(localRepository.classInstances);
-
-                })
-                .catch((error: any) => failureFunction(error));
-        });
-
-        return observable;
-    }
-
-
-    // TODO: not working as intended...
-    // removeClassInstances(volunteer: Volunteer, classInstances: ClassInstanceDTO[]) {
-    //     const observable = new Observable(subscriber => {
-    //         const failureFunction = (error: any) => {
-    //             subscriber.error(error);
-    //             subscriber.complete();
-    //         };
-
-    //         this.findByVolunteer(volunteer)
-    //             .toPromise()
-    //             .then((localRepository: LocalRepository) => {
-                  
-    //                 // localRepository.classInstances.forEach((ci, index, object) => {
-    //                 //     classInstances.forEach(t => {
-    //                 //         if (ci.id === t.id) {
-    //                 //             object.splice(index, 1);
-    //                 //         }
-    //                 //     });
-    //                 // });
-
-    //                 this.http.put(`${this.apiUrl}/${localRepository.id}`, localRepository)
-    //                     .toPromise()
-    //                     .then(() => subscriber.complete())
-    //                     .catch((error: any) => failureFunction(error));
-
-    //                 subscriber.next(localRepository.classInstances);
-
-    //             })
-    //             .catch((error: any) => failureFunction(error));
-    //     });
-
-    //     return observable;
-    // }
-
+  //     return observable;
+  // }
 }
