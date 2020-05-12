010bd4186acd00df62b6efca1d2e81c375814742
ntsmwk <markus.weissenbek@gmx.at>
2020-05-12 13:15:04 +0200

    fixed task-select

--- marketplace/marketplace-core/src/main/java/at/jku/cis/iVolunteer/marketplace/meta/core/class_/ClassDefinitionController.java
+++ marketplace/marketplace-core/src/main/java/at/jku/cis/iVolunteer/marketplace/meta/core/class_/ClassDefinitionController.java
@@ -8,10 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import at.jku.cis.iVolunteer.model.matching.MatchingCollector;
 import at.jku.cis.iVolunteer.model.meta.core.clazz.ClassArchetype;
 import at.jku.cis.iVolunteer.model.meta.core.clazz.ClassDefinition;
 import at.jku.cis.iVolunteer.model.meta.form.EnumEntry;
