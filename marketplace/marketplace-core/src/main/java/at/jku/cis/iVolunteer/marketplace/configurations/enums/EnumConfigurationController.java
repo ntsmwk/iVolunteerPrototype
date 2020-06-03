2ccd1e2b7fde7435f585d0ac2af06e647e478223
alexander <alexander.kopp@gmx.at>
2020-06-03 14:43:07 +0200

    added enum-configuration service

--- marketplace/marketplace-core/src/main/java/at/jku/cis/iVolunteer/marketplace/configurations/enums/EnumConfigurationController.java
+++ marketplace/marketplace-core/src/main/java/at/jku/cis/iVolunteer/marketplace/configurations/enums/EnumConfigurationController.java
@@ -1,12 +1,9 @@
 package at.jku.cis.iVolunteer.marketplace.configurations.enums;
 
-import java.util.ArrayList;
-import java.util.Date;
+
 import java.util.List;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,28 +13,55 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import at.jku.cis.iVolunteer.marketplace._mapper.property.PropertyDefinitionToClassPropertyMapper;
-import at.jku.cis.iVolunteer.marketplace.configurations.matching.collector.MatchingCollectorConfigurationRepository;
-import at.jku.cis.iVolunteer.marketplace.meta.core.class_.ClassDefinitionRepository;
-import at.jku.cis.iVolunteer.marketplace.meta.core.class_.ClassDefinitionService;
-import at.jku.cis.iVolunteer.marketplace.meta.core.class_.CollectionService;
-import at.jku.cis.iVolunteer.marketplace.meta.core.property.PropertyDefinitionRepository;
-import at.jku.cis.iVolunteer.marketplace.meta.core.relationship.RelationshipRepository;
-import at.jku.cis.iVolunteer.model.configurations.clazz.ClassConfiguration;
-import at.jku.cis.iVolunteer.model.configurations.matching.collector.MatchingCollectorConfiguration;
-import at.jku.cis.iVolunteer.model.matching.MatchingCollector;
-import at.jku.cis.iVolunteer.model.meta.core.clazz.ClassArchetype;
-import at.jku.cis.iVolunteer.model.meta.core.clazz.ClassDefinition;
-import at.jku.cis.iVolunteer.model.meta.core.property.definition.ClassProperty;
-import at.jku.cis.iVolunteer.model.meta.core.property.definition.PropertyDefinition;
-import at.jku.cis.iVolunteer.model.meta.core.relationship.Inheritance;
-import at.jku.cis.iVolunteer.model.meta.core.relationship.Relationship;
-import at.jku.cis.iVolunteer.model.meta.core.relationship.RelationshipType;
+import at.jku.cis.iVolunteer.model.configurations.enums.EnumConfiguration;
 
 @RestController
 public class EnumConfigurationController {
-
 	
+	@Autowired EnumConfigurationRepository enumConfigurationRepository;
+
+	@GetMapping("enum-configuration/all")
+	private List<EnumConfiguration> getAllEnumConfigurations() {
+
+		return enumConfigurationRepository.findAll();
+	}
+	
+	@GetMapping("enum-configuration/all/{tenantId}")
+	private List<EnumConfiguration> getAllEnumConfigurationsForTenant(@PathVariable("tenantId") String tenantId) {
+		return enumConfigurationRepository.findByTenantId(tenantId);
+	}
+	
+	@GetMapping("enum-configuration/{id}")
+	private EnumConfiguration getEnumConfigurationById(@PathVariable("id") String id) {
+
+		return enumConfigurationRepository.findOne(id);
+	}
+	
+	@GetMapping("enum-configuration/by-name/{name}")
+	private EnumConfiguration getEnumConfigurationByName(@PathVariable("name") String name) {
+		return enumConfigurationRepository.findByName(name);
+	}
+	
+	@PostMapping("enum-configuration/new")
+	private EnumConfiguration newEnumConfiguration(@RequestBody EnumConfiguration enumConfiguration) {
+		return enumConfigurationRepository.save(enumConfiguration);
+	}
+	
+	@PostMapping("enum-configuration/new-empty")
+	private EnumConfiguration newEmptyEnumConfiguration(@RequestBody String[] params) {
+		EnumConfiguration enumConfiguration = new EnumConfiguration(params[0], params[1]);
+		return enumConfigurationRepository.save(enumConfiguration);
+	}
+		
+	@PutMapping("enum-configuration/{id}/save") 
+	private EnumConfiguration replaceEnumConfiguration(@RequestBody EnumConfiguration enumConfiguration) {
+		return enumConfigurationRepository.save(enumConfiguration);
+	}
+	
+	@DeleteMapping("enum-configuration/{id}/delete")
+	private void deleteEnumConfiguration(@PathVariable("id") String id) {
+		enumConfigurationRepository.delete(id);
+	}
 	
 	
 }
