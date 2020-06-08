3e393bf26c2b3d9780ef0b708fb5e485d81ab268
ClaudiaVo <claudia.vojinovic@gmx.at>
2020-06-08 08:34:45 +0200

    Merge commondev into drools

--- core/core-application/src/main/java/at/jku/cis/iVolunteer/initialize/CoreVolunteerInitializationService.java
+++ core/core-application/src/main/java/at/jku/cis/iVolunteer/initialize/CoreVolunteerInitializationService.java
@@ -46,21 +46,21 @@ public class CoreVolunteerInitializationService {
 
 	public void initVolunteers(){
 
-		createVolunteer(BROISER, RAW_PASSWORD, "Berthold", "Roiser", "07.09.1988", "", "");
-		createVolunteer(PSTARZER, RAW_PASSWORD, "Philipp", "Starzer", "09.10.1995", "", "img/pstarzer.jpg");
-		createVolunteer(MWEISSENBEK, RAW_PASSWORD, "Markus", "Weißenbek", "23.01.1994", "", "");
-		createVolunteer(MWEIXLBAUMER, RAW_PASSWORD, "Markus", "Weixlbaumer", "24.05.1985", "", "img/weixlbaumer_small.png");
+		createVolunteer(BROISER, RAW_PASSWORD, "Berthold", "Roiser", "1988-09-07T00:00:00", "", "");
+		createVolunteer(PSTARZER, RAW_PASSWORD, "Philipp", "Starzer", "1995-10-09T00:00:00", "", "img/pstarzer.jpg");
+		createVolunteer(MWEISSENBEK, RAW_PASSWORD, "Markus", "Weißenbek", "1994-01-23T00:00:00", "", "");
+		createVolunteer(MWEIXLBAUMER, RAW_PASSWORD, "Markus", "Weixlbaumer", "1985-05-24T00:00:00", "", "img/weixlbaumer_small.png");
 
-		createVolunteer("AKop", "passme", "Alexander", "Kopp", "09.07.1998", "Alex", "");
-		createVolunteer("WRet", "passme", "Werner", "Retschitzegger", "04.11.1975", "", "");
-		createVolunteer("WSch", "passme", "Wieland", "Schwinger", "09.06.1976", "", "");
-		createVolunteer("BProe", "passme", "Birgit", "Pröll", "05.10.1976","", "");
-		createVolunteer("KKof", "passme", "Katharina", "Kofler", "08.05.1998","Kati", "");
-		createVolunteer("CVoj", "passme", "Claudia", "Vojinovic", "01.12.1981", "", "");
-		createVolunteer("KBauer", "passme", "Kerstin", "Bauer", "17.02.1960 09:00", "", "");
-		createVolunteer("EWagner", "passme", "Erich", "Wagner", "11.07.1980 09:00", "", "");
-		createVolunteer("WHaube", "passme", "Werner", "Haube", "08.08.1970 09:00", "", "");
-		createVolunteer("MJachs", "passme", "Melanie", "Jachs", "08.07.1970 09:00", "", "");		
+		createVolunteer("AKop", "passme", "Alexander", "Kopp", "1998-07-09T00:00:00", "Alex", "");
+		createVolunteer("WRet", "passme", "Werner", "Retschitzegger", "1975-11-04T00:00:00", "", "");
+		createVolunteer("WSch", "passme", "Wieland", "Schwinger", "1976-06-09T00:00:00", "", "");
+		createVolunteer("BProe", "passme", "Birgit", "Pröll", "1976-10-05T00:00:00","", "");
+		createVolunteer("KKof", "passme", "Katharina", "Kofler", "1998-05-08T00:00:00","Kati", "");
+		createVolunteer("CVoj", "passme", "Claudia", "Vojinovic", "1981-12-01T00:00:00", "", "");
+		createVolunteer("KBauer", "passme", "Kerstin", "Bauer", "1960-02-17T00:00:00", "", "");
+		createVolunteer("EWagner", "passme", "Erich", "Wagner", "1980-07-11T00:00:00", "", "");
+		createVolunteer("WHaube", "passme", "Werner", "Haube", "1970-08-08T00:00:00", "", "");
+		createVolunteer("MJachs", "passme", "Melanie", "Jachs", "1970-07-08T00:00:00", "", "");		
 	}
 
 	private CoreVolunteer createVolunteer(String username, String password, String firstName, String lastName, String birthDate,
