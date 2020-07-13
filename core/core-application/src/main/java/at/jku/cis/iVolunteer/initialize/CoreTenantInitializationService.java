a694508c5703656509a0333608fe96365e3f391c
ntsmwk <markus.weissenbek@gmx.at>
2020-07-13 10:54:48 +0200

    tenant/user image should work now properly fixed #102

--- core/core-application/src/main/java/at/jku/cis/iVolunteer/initialize/CoreTenantInitializationService.java
+++ core/core-application/src/main/java/at/jku/cis/iVolunteer/initialize/CoreTenantInitializationService.java
@@ -4,6 +4,8 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 import javax.imageio.ImageIO;
@@ -12,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
 
 import at.jku.cis.iVolunteer.core.marketplace.MarketplaceRepository;
 import at.jku.cis.iVolunteer.core.tenant.TenantRepository;
@@ -31,15 +34,18 @@ public class CoreTenantInitializationService {
 	public void initTenants() {
 		Marketplace marketplace = marketplaceRepository.findByName("Marketplace 1");
 		if (marketplace != null) {
-			createTenant(FF_EIDENBERG, "www.ff-eidenberg.at", "/img/FF_Altenberg.jpg", "#b20000", "#b2b2b2", marketplace.getId());
-			createTenant(MV_SCHWERTBERG, "www.musikverein-schwertberg.at", "/img/musikvereinschwertberg.jpeg", "#005900", "#b2b2b2", marketplace.getId());
-			createTenant(RK_WILHERING, "www.roteskreuz.at/ooe/dienststellen/eferding/die-bezirksstelle/die-ortsstellen/wilhering", "/img/OERK_Sonderlogo_rgb_cropped.jpg", "#b2b2b2", "#b2b2b2",
+			createTenant(FF_EIDENBERG, "www.ff-eidenberg.at", "/img/FF_Altenberg.jpg", "#b20000", "#b2b2b2",
 					marketplace.getId());
+			createTenant(MV_SCHWERTBERG, "www.musikverein-schwertberg.at", "/img/musikvereinschwertberg.jpeg",
+					"#005900", "#b2b2b2", marketplace.getId());
+			createTenant(RK_WILHERING,
+					"www.roteskreuz.at/ooe/dienststellen/eferding/die-bezirksstelle/die-ortsstellen/wilhering",
+					"/img/OERK_Sonderlogo_rgb_cropped.jpg", "#b2b2b2", "#b2b2b2", marketplace.getId());
 		}
 	}
 
-	private Tenant createTenant(String name, String homepage, String fileName, String primaryColor, String secondaryColor,
-			String marketplaceId) {
+	private Tenant createTenant(String name, String homepage, String fileName, String primaryColor,
+			String secondaryColor, String marketplaceId) {
 		Tenant tenant = coreTenantRepository.findByName(name);
 
 		if (tenant == null) {
@@ -57,15 +63,10 @@ public class CoreTenantInitializationService {
 
 	private void setTenantImage(String fileName, Tenant tenant) {
 		if (fileName != null && !fileName.equals("")) {
+			ClassPathResource classPathResource = new ClassPathResource(fileName);
 			try {
-				InputStream resourceAsStream = this.getClass().getResourceAsStream(fileName);
-				
-			    byte[] targetArray = new byte[resourceAsStream.available()];
-			    resourceAsStream.read(targetArray);
-				tenant.setImage(targetArray);
-//				Resource resource = new ClassPathResource(fileName);
-//				File file = resource.getFile();
-//				tenant.setImage(Files.readAllBytes(file.toPath()));
+				byte[] binaryData = FileCopyUtils.copyToByteArray(classPathResource.getInputStream());
+				tenant.setImage(binaryData);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
