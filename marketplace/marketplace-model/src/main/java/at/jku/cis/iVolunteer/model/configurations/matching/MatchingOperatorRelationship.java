c8127a7c2b17ae3103e25fbd6571d567bf30fc6b
ntsmwk <markus.weissenbek@gmx.at>
2020-07-13 12:28:34 +0200

    ..

--- marketplace/marketplace-model/src/main/java/at/jku/cis/iVolunteer/model/configurations/matching/MatchingOperatorRelationship.java
+++ marketplace/marketplace-model/src/main/java/at/jku/cis/iVolunteer/model/configurations/matching/MatchingOperatorRelationship.java
@@ -3,94 +3,110 @@ package at.jku.cis.iVolunteer.model.configurations.matching;
 import org.springframework.data.annotation.Id;
 
 public class MatchingOperatorRelationship {
-	
+
 	@Id String id;
 
 	String producerPath;
-	MatchingProducerConsumerType producerType;
-	
+	MatchingEntityType producerType;
+
 	String consumerPath;
-	MatchingProducerConsumerType consumerType;
-	
+	MatchingEntityType consumerType;
+
 	MatchingOperatorType matchingOperatorType;
-	
+
 	float weighting;
 	boolean necessary;
 	float fuzzyness;
-	
+
 	int coordX;
 	int coordY;
-	
-	
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getProducerPath() {
 		return producerPath;
 	}
+
 	public void setProducerPath(String producerPath) {
 		this.producerPath = producerPath;
 	}
-	public MatchingProducerConsumerType getProducerType() {
+
+	public MatchingEntityType getProducerType() {
 		return producerType;
 	}
-	public void setProducerType(MatchingProducerConsumerType producerType) {
+
+	public void setProducerType(MatchingEntityType producerType) {
 		this.producerType = producerType;
 	}
+
 	public String getConsumerPath() {
 		return consumerPath;
 	}
+
 	public void setConsumerPath(String consumerPath) {
 		this.consumerPath = consumerPath;
 	}
-	public MatchingProducerConsumerType getConsumerType() {
+
+	public MatchingEntityType getConsumerType() {
 		return consumerType;
 	}
-	public void setConsumerType(MatchingProducerConsumerType consumerType) {
+
+	public void setConsumerType(MatchingEntityType consumerType) {
 		this.consumerType = consumerType;
 	}
+
 	public MatchingOperatorType getMatchingOperatorType() {
 		return matchingOperatorType;
 	}
+
 	public void setMatchingOperatorType(MatchingOperatorType matchingOperatorType) {
 		this.matchingOperatorType = matchingOperatorType;
 	}
+
 	public float getWeighting() {
 		return weighting;
 	}
+
 	public void setWeighting(float weighting) {
 		this.weighting = weighting;
 	}
+
 	public boolean isNecessary() {
 		return necessary;
 	}
+
 	public void setNecessary(boolean necessary) {
 		this.necessary = necessary;
 	}
+
 	public float getFuzzyness() {
 		return fuzzyness;
 	}
+
 	public void setFuzzyness(float fuzzyness) {
 		this.fuzzyness = fuzzyness;
 	}
+
 	public int getCoordX() {
 		return coordX;
 	}
+
 	public void setCoordX(int coordX) {
 		this.coordX = coordX;
 	}
+
 	public int getCoordY() {
 		return coordY;
 	}
+
 	public void setCoordY(int coordY) {
 		this.coordY = coordY;
 	}
-	
-	
-	
-	
-	
+
 }
