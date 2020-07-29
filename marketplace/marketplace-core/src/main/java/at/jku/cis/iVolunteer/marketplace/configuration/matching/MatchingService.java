c40306f68c427fb94d4a4af94599f5082ba73a86
ntsmwk <markus.weissenbek@gmx.at>
2020-07-29 13:22:18 +0200

    ...

--- marketplace/marketplace-core/src/main/java/at/jku/cis/iVolunteer/marketplace/configuration/matching/MatchingService.java
+++ marketplace/marketplace-core/src/main/java/at/jku/cis/iVolunteer/marketplace/configuration/matching/MatchingService.java
@@ -3,6 +3,7 @@ package at.jku.cis.iVolunteer.marketplace.configuration.matching;
 import static at.jku.cis.iVolunteer.marketplace.meta.core.class_.CollectionService.PATH_DELIMITER;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ import at.jku.cis.iVolunteer.marketplace.meta.core.class_.ClassInstanceRepositor
 import at.jku.cis.iVolunteer.model.configurations.matching.MatchingOperatorRelationship;
 import at.jku.cis.iVolunteer.model.meta.core.clazz.ClassDefinition;
 import at.jku.cis.iVolunteer.model.meta.core.clazz.ClassInstance;
+import at.jku.cis.iVolunteer.model.meta.core.property.definition.ClassProperty;
 
 @Service
 public class MatchingService {
@@ -30,28 +32,72 @@ public class MatchingService {
 		List<ClassDefinition> classDefinitions = classDefinitionRepository.findByTenantId(tenantId);
 
 		for (MatchingOperatorRelationship relationship : relationships) {
-			String[] leftPathSplitted = relationship.getLeftMatchingEntityPath().split(PATH_DELIMITER);
-			switch (relationship.getLeftMatchingEntityType()) {
-			case PROPERTY:
-				if (leftPathSplitted.length > 1) {
-					String classId = leftPathSplitted[leftPathSplitted.length - 2];
-					String propertyId = leftPathSplitted[leftPathSplitted.length - 1];
+			ClassProperty<Object> leftClassProperty = null;
+			ClassProperty<Object> rightClassProperty = null;
 
-					System.out.println(classId);
-					System.out.println(propertyId);
-					ClassDefinition classDeifinition = classDefinitions.stream().filter(cd -> cd.getId().equals(classId)).findFirst().orElse(null);
-					
-					
-				} else {
-					throw new RuntimeException("left path too short");
-				}
-				break;
-			case CLASS:
-				break;
-			}
+			
+			final ClassDefinition leftClassDefinition = handleLeftEntity(classDefinitions, relationship);
+			
+			final ClassDefinition rightClassDefinition = handleRightEntity(classDefinitions, relationship);
+			
+			
+			classInstances.stream().filter(ci -> ci.getClassDefinitionId().equals(leftClassDefinition.getId())).collect(Collectors.toList());
+			
+			
+		
 
 		}
 
+	}
+
+	private ClassDefinition handleLeftEntity(List<ClassDefinition> classDefinitions,
+			MatchingOperatorRelationship relationship) {
+		ClassProperty<Object> leftClassProperty;
+		ClassDefinition leftClassDefinition = null;
+		String[] leftPathSplitted = relationship.getLeftMatchingEntityPath().split(PATH_DELIMITER);
+		switch (relationship.getLeftMatchingEntityType()) {
+		case PROPERTY:
+			if (leftPathSplitted.length > 1) {
+				String classId = leftPathSplitted[leftPathSplitted.length - 2];
+				String propertyId = leftPathSplitted[leftPathSplitted.length - 1];
+
+				leftClassDefinition = classDefinitions.stream().filter(cd -> cd.getId().equals(classId)).findFirst()
+						.orElse(null);
+				leftClassProperty = leftClassDefinition.getProperties().stream()
+						.filter(cp -> cp.getId().equals(propertyId)).findFirst().orElse(null);
+			} else {
+				throw new RuntimeException("left path too short");
+			}
+			break;
+		case CLASS:
+			break;
+		}
+		return leftClassDefinition;
+	}
+
+	private ClassDefinition handleRightEntity(List<ClassDefinition> classDefinitions,
+			MatchingOperatorRelationship relationship) {
+		ClassDefinition rightClassDefinition = null;
+		ClassProperty<Object> rightClassProperty;
+		String[] rightPathSplitted = relationship.getRightMatchingEntityPath().split(PATH_DELIMITER);
+		switch (relationship.getRightMatchingEntityType()) {
+		case PROPERTY:
+			if (rightPathSplitted.length > 1) {
+				String classId = rightPathSplitted[rightPathSplitted.length - 2];
+				String propertyId = rightPathSplitted[rightPathSplitted.length - 1];
+
+				rightClassDefinition = classDefinitions.stream().filter(cd -> cd.getId().equals(classId)).findFirst()
+						.orElse(null);
+				rightClassProperty = rightClassDefinition.getProperties().stream()
+						.filter(cp -> cp.getId().equals(propertyId)).findFirst().orElse(null);
+			} else {
+				throw new RuntimeException("right path too short");
+			}
+			break;
+		case CLASS:
+			break;
+		}
+		return rightClassDefinition;
 	}
 
 	public float match(List<ClassInstance> classInstances, ClassInstance ci) {
