1c34b8130bb237cab82c724856e42b9ee324dcad
pstarzer <philipp.starzer@gmail.com>
2020-08-06 11:33:37 +0200

    show 'Organisationen verwalten' only for volunteers and helpseekers

--- marketplace-frontend/src/app/main/toolbar/user-menu/user-menu.component.ts
+++ marketplace-frontend/src/app/main/toolbar/user-menu/user-menu.component.ts
@@ -1,27 +1,43 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { User, UserRole } from "../../content/_model/user";
 import { LoginService } from "../../content/_service/login.service";
 import { isNullOrUndefined } from "util";
 import { ImageService } from "app/main/content/_service/image.service";
 import { Router } from "@angular/router";
+import { Subscription } from "rxjs";
+import { RoleChangeService } from "app/main/content/_service/role-change.service";
 
 @Component({
   selector: "fuse-user-menu",
   templateUrl: "./user-menu.component.html",
   styleUrls: ["./user-menu.component.scss"],
 })
-export class FuseUserMenuComponent implements OnInit {
+export class FuseUserMenuComponent implements OnInit, OnDestroy {
   user: User;
   role: UserRole;
+
+  onRoleChanged: Subscription;
 
   constructor(
     private router: Router,
     private loginService: LoginService,
-    private imageService: ImageService
-  ) {}
+    private imageService: ImageService,
+    private roleChangeService: RoleChangeService
+  ) {
+    this.onRoleChanged = this.roleChangeService.onRoleChanged.subscribe(() => {
+      this.ngOnInit();
+    });
+  }
 
   async ngOnInit() {
+    this.role = <UserRole>(
+      await this.loginService.getLoggedInUserRole().toPromise()
+    );
     this.user = <User>await this.loginService.getLoggedIn().toPromise();
+  }
+
+  ngOnDestroy() {
+    this.onRoleChanged.unsubscribe();
   }
 
   logout() {
