4517e635062f684154fbadb46f739f71b032cc99
ntsmwk <markus.weissenbek@gmx.at>
2020-09-24 15:56:57 +0200

    ..

--- marketplace/marketplace-core/src/main/java/at/jku/cis/iVolunteer/marketplace/task/TaskDefinitionController.java
+++ marketplace/marketplace-core/src/main/java/at/jku/cis/iVolunteer/marketplace/task/TaskDefinitionController.java
@@ -9,18 +9,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import at.jku.cis.iVolunteer.marketplace.meta.core.class_.ClassDefinitionService;
+import at.jku.cis.iVolunteer.marketplace.meta.core.class_.ClassInstanceService;
 import at.jku.cis.iVolunteer.model.meta.core.clazz.ClassArchetype;
 import at.jku.cis.iVolunteer.model.meta.core.clazz.ClassDefinition;
+import at.jku.cis.iVolunteer.model.meta.core.clazz.ClassInstance;
 
 @Controller
 @RequestMapping("/tasktemplate")
 public class TaskDefinitionController {
 
-	@Autowired private ClassDefinitionService classDefinitionService;
+	@Autowired private ClassInstanceService classInstanceService;
 	
 	@GetMapping("/tenant/{tenantId}")
-	public List<ClassDefinition> getTaskClassDefinitionsByTenantId(@PathVariable String tenantId){
+	public List<ClassInstance> getTaskClassDefinitionsByTenantId(@PathVariable String tenantId){
 //		TODO ALEX Mapper
-		return classDefinitionService.getClassDefinitionsByArchetype(ClassArchetype.TASK, tenantId);
+		return classInstanceService.getClassInstanceByArchetype(ClassArchetype.TASK, tenantId);
 	}
 }
