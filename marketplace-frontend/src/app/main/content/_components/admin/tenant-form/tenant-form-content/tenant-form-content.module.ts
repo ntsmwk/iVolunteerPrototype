a8d4b95610b1810bcb347fdbafb886bd70a3e378
alexander <alexander.kopp@gmx.at>
2020-10-01 14:11:48 +0200

    moved image uploaders from tenant-form-content to shared/uploaders

--- marketplace-frontend/src/app/main/content/_components/admin/tenant-form/tenant-form-content/tenant-form-content.module.ts
+++ marketplace-frontend/src/app/main/content/_components/admin/tenant-form/tenant-form-content/tenant-form-content.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from "@angular/core";
-import { TenantFormContentComponent } from "./tenant-form-content.component";
+import { NgModule } from '@angular/core';
+import { TenantFormContentComponent } from './tenant-form-content.component';
 import {
   MatButtonModule, MatIconModule, MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, MatInputModule,
-  MatTableModule, MatProgressSpinnerModule,
-} from "@angular/material";
-import { FuseSharedModule } from "@fuse/shared.module";
+  MatProgressSpinnerModule,
+} from '@angular/material';
+import { FuseSharedModule } from '@fuse/shared.module';
 import { MaterialFileInputModule } from 'ngx-material-file-input';
 import { TenantTagFormModule } from './tag-form/tag-form.module';
-import { TenantHelpseekersFormModule } from './helpseekers-form/helpseekers-form.module';
-import { ProfileImageUploadModule } from './profile-image-upload/profile-image-upload.module';
-import { LandingPageImageUploadModule } from './landing-page-image-upload/landing-page-image-upload.module';
+import { ProfileImageUploadModule } from '../../../_shared/uploaders/profile-image-upload/profile-image-upload.module';
+import { LandingPageImageUploadModule } from '../../../_shared/uploaders/landing-page-image-upload/landing-page-image-upload.module';
 
 
 @NgModule({
