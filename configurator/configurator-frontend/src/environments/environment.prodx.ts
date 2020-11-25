22c2c239e6b8365a7b72865f5c0ed35a6842dc68
ntsmwk <markus.weissenbek@gmx.at>
2020-11-25 12:23:45 +0100

    ..

---
+++ configurator/configurator-frontend/src/environments/environment.prodx.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  hmr: false,
+  REDIRECT_URI: "https://app-jku.ivolunteer.at/main/profile",
+  JSON_SERVER_URL: "http://172.24.200.171:3000/repository",
+  CORE_URL: "http://172.24.200.171:8090",
+  CONFIGURATOR_URL: "http://172.24.200.171:8095",
+  MODE: "iVolunteer" // 'iVolunteer' || 'flexprod' || 'all'
+};
