b025ed2bf8467e3fbed9d01e0cadce0c73f4aed5
alexander <alexander.kopp@gmx.at>
2020-11-25 13:15:59 +0100

    task-select added dropdown

--- configurator/configurator-frontend/src/app/main/content/_components/task-select/task-select.component.ts
+++ configurator/configurator-frontend/src/app/main/content/_components/task-select/task-select.component.ts
@@ -8,16 +8,20 @@ import {
   ClassDefinition,
   ClassArchetype
 } from "../../_model/configurator/class";
+import { environment } from "environments/environment";
 
 @Component({
   templateUrl: "./task-select.component.html",
   styleUrls: ["./task-select.component.scss"]
 })
 export class FuseTaskSelectComponent implements OnInit {
+  allClassDefinitions: ClassDefinition[];
   dataSource = new MatTableDataSource<ClassDefinition>();
   displayedColumns = ["name", "configuration"];
   tenantId: string;
   redirectUrl: string;
+  environmentMode: string;
+  dropdownFilterValue: string;
 
   constructor(
     formBuilder: FormBuilder,
@@ -27,6 +31,9 @@ export class FuseTaskSelectComponent implements OnInit {
   ) { }
 
   async ngOnInit() {
+    this.environmentMode = environment.MODE;
+
+
     this.route.queryParams.subscribe(params => {
       if (
         isNullOrUndefined(params["tenantId"]) ||
@@ -38,15 +45,34 @@ export class FuseTaskSelectComponent implements OnInit {
         this.redirectUrl = params["redirect"];
       }
     });
-    const tasks = <ClassDefinition[]>(
-      await this.classDefinitionService
-        .getByArchetypes([ClassArchetype.TASK, ClassArchetype.FLEXPROD], this.tenantId)
-        .toPromise()
-    );
 
-    this.dataSource.data = tasks
+    let archetypes: ClassArchetype[] = [];
+    if (environment.MODE === 'flexprod') {
+      archetypes = [ClassArchetype.FLEXPROD]
+    } else if (environment.MODE === 'iVolunteer') {
+      archetypes = [ClassArchetype.TASK, ClassArchetype.FUNCTION, ClassArchetype.ACHIEVEMENT, ClassArchetype.COMPETENCE];
+    } else {
+      archetypes = [ClassArchetype.TASK, ClassArchetype.FUNCTION, ClassArchetype.ACHIEVEMENT, ClassArchetype.COMPETENCE, ClassArchetype.FLEXPROD];
+    }
+
+
+    this.allClassDefinitions = <ClassDefinition[]>await this.classDefinitionService
+      .getByArchetypes(archetypes, this.tenantId).toPromise();
+    this.dropdownFilterValue = 'ALL';
+
+
+    this.dataSource.data = this.allClassDefinitions
       .filter(t => t.configurationId != null)
       .sort((c1, c2) => c1.configurationId.localeCompare(c2.configurationId));
+  }
+
+  applyArchetypeFilter() {
+    if (this.dropdownFilterValue === 'ALL') {
+      this.dataSource.data = this.allClassDefinitions;
+    } else {
+      this.dataSource.data = this.allClassDefinitions.filter(cd => cd.classArchetype === this.dropdownFilterValue);
+    }
+
   }
 
   onRowSelect(row) {
